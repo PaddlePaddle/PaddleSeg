3b0b49d6efd0a586b783c0168d5c297751ed9dea
joey12300 <136876878@qq.com>
2019-11-04 14:40:58 +0800

    change to python google style

--- inference/tools/visualize.py
+++ inference/tools/visualize.py
@@ -8,13 +8,14 @@ color_map = [[128, 64, 128], [244, 35, 231], [69, 69, 69], [102, 102, 156],
              [255, 0, 0], [0, 0, 142], [0, 0, 69], [0, 60, 100], [0, 79, 100],
              [0, 0, 230], [119, 10, 32]]
 # python visualize.py demo1.jpg demo1_jpg.png vis_result.png
-if __name__ == "__main__": 
+if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: python visualize.py demo1.jpg demo1_jpg.png vis_result.png")
+        print(
+            "Usage: python visualize.py demo1.jpg demo1_jpg.png vis_result.png")
     else:
         ori_im = cv2.imread(sys.argv[1])
         ori_shape = ori_im.shape
-        print(ori_shape) 
+        print(ori_shape)
         im = cv2.imread(sys.argv[2])
         shape = im.shape
         print("visualizing...")
