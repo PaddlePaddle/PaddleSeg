9c5c6055dfb07ce393f901de8d65447b67c6c729
chulutao <chulutao@baidu.com>
2019-11-15 11:43:04 +0800

    modify labelme jingling

--- docs/annotation/labelme2seg.md
+++ docs/annotation/labelme2seg.md
@@ -47,7 +47,7 @@ git clone https://github.com/wkentaro/labelme
 
 ​		(3)   图片中所有目标的标注都完成后，点击`Save`保存json文件，**请将json文件和图片放在同一个文件夹里**，点击`Next Image`标注下一张图片。
 
-LableMe产出的真值文件可参考我们给出的文件夹`data_annotated`。
+LableMe产出的真值文件可参考我们给出的文件夹`docs/annotation/labelme_demo`。
 
 <div align="center">
     <img src="../imgs/annotation/image-5.png" width="600px"/>
@@ -65,21 +65,24 @@ LableMe产出的真值文件可参考我们给出的文件夹`data_annotated`。
 
  ## 3 数据格式转换
 
-* 我们用于完成语义分割的数据集目录结构如下：
+* 经过数据格式转换后的数据集目录结构如下：
 
  ```
  my_dataset                 # 根目录 
- |-- JPEGImages             # 数据集图片
- |-- SegmentationClassPNG   # 数据集真值 
+ |-- annotations            # 数据集真值 
  |   |-- xxx.png            # 像素级别的真值信息 
  |   |... 
  |-- class_names.txt        # 数据集的类别名称
+ |-- xxx.jpg(png or other)  # 数据集原图
+ |-- ...
+ |-- xxx.json               # 标注json文件
+ |-- ...
  
  ```
 
 <div align="center">
     <img src="../imgs/annotation/image-6.png" width="600px"/>
-    <p>图7 训练所需的数据集目录的结构示意图</p>
+    <p>图7 格式转换后的数据集目录的结构示意图</p>
  </div>
 
 * 运行转换脚本需要依赖labelme和pillow，如未安装，请先安装。Labelme的具体安装流程请参见[官方安装指南](https://github.com/wkentaro/labelme)。Pillow的安装：
@@ -91,14 +94,14 @@ pip install pillow
 * 运行以下代码，将标注后的数据转换成满足以上格式的数据集：
 
 ```
-  python docs/annotation/labelme2seg.py <path/to/label_json_file> <path/to/output_dataset>
+  python pdseg/tools/labelme2seg.py <path/to/label_json_file>
 ```
 
-其中，`<path/to/label_json_files>`为图片以及LabelMe产出的json文件所在文件夹的目录，`<path/to/output_dataset>`为转换后的数据集所在文件夹的目录。**需注意的是：`<path/to/output_dataset>`不用预先创建，脚本运行时会自动创建，否则会报错。**
+其中，`<path/to/label_json_files>`为图片以及LabelMe产出的json文件所在文件夹的目录，同时也是转换后的标注集所在文件夹的目录。
 
-转换得到的数据集可参考我们给出的文件夹`my_dataset`。其中，文件`class_names.txt`是数据集中所有标注类别的名称，包含背景类；文件夹`JPEGImages`保存的是数据集的图片；文件夹`SegmentationClassPNG`保存的是各图片的像素级别的真值信息，背景类`_background_`对应为0，其它目标类别从1开始递增，至多为255。
+转换得到的数据集可参考我们给出的文件夹`docs/annotation/labelme_demo`。其中，文件`class_names.txt`是数据集中所有标注类别的名称，包含背景类；文件夹`annotations`保存的是各图片的像素级别的真值信息，背景类`_background_`对应为0，其它目标类别从1开始递增，至多为255。
 
 <div align="center">
     <img src="../imgs/annotation/image-7.png" width="600px"/>
-    <p>图8 训练所需的数据集各目录的内容示意图</p>
+    <p>图8 格式转换后的数据集各目录的内容示意图</p>
  </div>	
--- pdseg/tools/labelme2seg.py
+++ pdseg/tools/labelme2seg.py
@@ -7,7 +7,6 @@ import glob
 import json
 import os
 import os.path as osp
-import sys
 
 import numpy as np
 import PIL.Image
@@ -15,47 +14,20 @@ import PIL.Image
 import labelme
 
 
-def get_color_map_list(num_classes):
-    """ Returns the color map for visualizing the segmentation mask,
-        which can support arbitrary number of classes.
-    Args:
-        num_classes: Number of classes
-    Returns:
-        The color map
-    """
-    #color_map = num_classes * 3 *  [0]
-    color_map = num_classes * [0, 0, 0]
-    for i in range(0, num_classes):
-        j = 0
-        lab = i
-        while lab:
-            color_map[i*3] |= (((lab >> 0) & 1) << (7 - j))
-            color_map[i*3+1] |= (((lab >> 1) & 1) << (7 - j))
-            color_map[i*3+2] |= (((lab >> 2) & 1) << (7 - j))
-            j += 1
-            lab >>= 3
-
-    return color_map
-
 def parse_args():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     parser.add_argument('input_dir',
                         help='input annotated directory')
-    parser.add_argument('output_dir',
-                        help='output dataset directory')
     return parser.parse_args()
 
 
 def main(args):
-    if not osp.exists(args.output_dir):
-        # print('Output directory already exists:', args.output_dir)
-        # sys.exit(1)
-        os.makedirs(args.output_dir)
-        os.makedirs(osp.join(args.output_dir, 'JPEGImages'))
-        os.makedirs(osp.join(args.output_dir, 'SegmentationClassPNG'))
-        print('Creating dataset:', args.output_dir)
+    output_dir = osp.join(args.input_dir, 'annotations')
+    if not osp.exists(output_dir):
+        os.makedirs(output_dir)
+        print('Creating annotations directory:', output_dir)
 
     # get the all class names for the given dataset
     class_names = ['_background_']
@@ -71,34 +43,29 @@ def main(args):
 
     class_name_to_id = {}
     for i, class_name in enumerate(class_names):
-        class_id = i # starts with 0
+        class_id = i  # starts with 0
         class_name_to_id[class_name] = class_id
         if class_id == 0:
             assert class_name == '_background_'
     class_names = tuple(class_names)
     print('class_names:', class_names)
 
-    out_class_names_file = osp.join(args.output_dir, 'class_names.txt')
+    out_class_names_file = osp.join(args.input_dir, 'class_names.txt')
     with open(out_class_names_file, 'w') as f:
         f.writelines('\n'.join(class_names))
     print('Saved class_names:', out_class_names_file)
-
-    color_map = get_color_map_list(256)
 
     for label_file in glob.glob(osp.join(args.input_dir, '*.json')):
         print('Generating dataset from:', label_file)
         with open(label_file) as f:
             base = osp.splitext(osp.basename(label_file))[0]
-            out_img_file = osp.join(
-                args.output_dir, 'JPEGImages', base + '.jpg')
             out_png_file = osp.join(
-                args.output_dir, 'SegmentationClassPNG', base + '.png')
+                output_dir, base + '.png')
 
             data = json.load(f)
 
             img_file = osp.join(osp.dirname(label_file), data['imagePath'])
             img = np.asarray(PIL.Image.open(img_file))
-            PIL.Image.fromarray(img).save(out_img_file)
 
             lbl = labelme.utils.shapes_to_label(
                 img_shape=img.shape,
@@ -108,11 +75,9 @@ def main(args):
 
             if osp.splitext(out_png_file)[1] != '.png':
                 out_png_file += '.png'
-            # Assume label ranses [0, 255] for uint8,
+            # Assume label ranges [0, 255] for uint8,
             if lbl.min() >= 0 and lbl.max() <= 255:
-                # lbl_pil = PIL.Image.fromarray(lbl.astype(np.uint8), mode='L')
-                lbl_pil = PIL.Image.fromarray(lbl.astype(np.uint8), mode='P')
-                lbl_pil.putpalette(color_map)
+                lbl_pil = PIL.Image.fromarray(lbl.astype(np.uint8), mode='L')
                 lbl_pil.save(out_png_file)
             else:
                 raise ValueError(
@@ -120,13 +85,7 @@ def main(args):
                     'Please consider using the .npy format.' % out_png_file
                 )
 
+
 if __name__ == '__main__':
     args = parse_args()
-
-    # args = argparse.ArgumentParser(
-    #     formatter_class=argparse.ArgumentDefaultsHelpFormatter
-    # )
-    # args.input_dir = '../../dataset/humanseg_labelme/'
-    # args.output_dir = '../../dataset/humanseg_labelme/LMode'
-
     main(args)
