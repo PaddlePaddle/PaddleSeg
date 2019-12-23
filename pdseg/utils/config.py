b22de1d1af373873de0a50f89238c700d9cddb25
LutaoChu <30695251+LutaoChu@users.noreply.github.com>
2019-12-23 16:59:33 +0800

    Update config.py

--- pdseg/utils/config.py
+++ pdseg/utils/config.py
@@ -72,17 +72,11 @@ cfg.DATASET.IGNORE_INDEX = 255
 cfg.DATASET.PADDING_VALUE = [127.5, 127.5, 127.5]
 
 ########################### 数据增强配置 ######################################
-# 图像镜像左右翻转
-cfg.AUG.MIRROR = True
-# 图像上下翻转开关，True/False
-cfg.AUG.FLIP = False
-# 图像启动上下翻转的概率，0-1
-cfg.AUG.FLIP_RATIO = 0.5
-# 图像resize的固定尺寸（宽，高），非负
-cfg.AUG.FIX_RESIZE_SIZE = tuple()
 # 图像resize的方式有三种：
 # unpadding（固定尺寸），stepscaling（按比例resize），rangescaling（长边对齐）
-cfg.AUG.AUG_METHOD = 'rangescaling'
+cfg.AUG.AUG_METHOD = 'unpadding'
+# 图像resize的固定尺寸（宽，高），非负
+cfg.AUG.FIX_RESIZE_SIZE = (512, 512)
 # 图像resize方式为stepscaling，resize最小尺度，非负
 cfg.AUG.MIN_SCALE_FACTOR = 0.5
 # 图像resize方式为stepscaling，resize最大尺度，不小于MIN_SCALE_FACTOR
@@ -97,6 +91,13 @@ cfg.AUG.MAX_RESIZE_VALUE = 600
 # 图像resize方式为rangescaling, 测试验证可视化模式下长边resize的长度，
 # 在MIN_RESIZE_VALUE到MAX_RESIZE_VALUE范围内
 cfg.AUG.INF_RESIZE_VALUE = 500
+
+# 图像镜像左右翻转
+cfg.AUG.MIRROR = True
+# 图像上下翻转开关，True/False
+cfg.AUG.FLIP = False
+# 图像启动上下翻转的概率，0-1
+cfg.AUG.FLIP_RATIO = 0.5
 
 # RichCrop数据增广开关，用于提升模型鲁棒性
 cfg.AUG.RICH_CROP.ENABLE = False
