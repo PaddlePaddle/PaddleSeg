031d89eeef5532e36687cb01de214fd198e7bfd8
LielinJiang <jianglielin@baidu.com>
2020-02-11 13:50:53 +0800

    fix nas python3 bug

--- slim/nas/mobilenetv2_search_space.py
+++ slim/nas/mobilenetv2_search_space.py
@@ -180,7 +180,7 @@ class MobileNetV2SpaceSeg(SearchSpaceBase):
                     c=int(c * self.scale),
                     n=n,
                     s=s,
-                    k=k,
+                    k=int(k),
                     name='mobilenetv2_conv' + str(i))
                 in_c = int(c * self.scale)
 
