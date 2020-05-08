cb1f79f05adfd5a9db4582f8057aae8ef7610c14
Channingss <chen_lingchi@163.com>
2020-05-08 02:45:44 +0000

    update linux_build.sh

--- contrib/RealTimeHumanSeg/cpp/linux_build.sh
+++ contrib/RealTimeHumanSeg/cpp/linux_build.sh
@@ -1,5 +1,5 @@
 OPENCV_URL=https://bj.bcebos.com/paddleseg/deps/opencv346gcc4.8contrib.tar.bz2
-if [ ! -d "./deps/opencv3gcc4.8" ]; then
+if [ ! -d "./deps/opencv346_with_contrib" ]; then
     mkdir -p deps
     cd deps
     wget -c ${OPENCV_URL}
