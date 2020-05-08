5b9a1db66fca876814c789360896e0409197ae5d
channings <chen_lingchi@163.com>
2020-05-08 15:05:58 +0800

    Fix bug of opencv for humanseg video deployment (#234)
    * fix bug of opencv
    
    * update linux_build.sh

--- contrib/RealTimeHumanSeg/cpp/linux_build.sh
+++ contrib/RealTimeHumanSeg/cpp/linux_build.sh
@@ -1,18 +1,18 @@
-OPENCV_URL=https://paddleseg.bj.bcebos.com/deploy/deps/opencv346.tar.bz2
-if [ ! -d "./deps/opencv346" ]; then
+OPENCV_URL=https://bj.bcebos.com/paddleseg/deps/opencv346gcc4.8contrib.tar.bz2
+if [ ! -d "./deps/opencv346_with_contrib" ]; then
     mkdir -p deps
     cd deps
     wget -c ${OPENCV_URL}
-    tar xvfj opencv346.tar.bz2
-    rm -rf opencv346.tar.bz2
+    tar xvfj opencv346gcc4.8contrib.tar.bz2
+    rm -rf opencv346gcc4.8contrib.tar.bz2
     cd ..
 fi
 
 WITH_GPU=OFF
-PADDLE_DIR=/root/projects/deps/fluid_inference/
+PADDLE_DIR=/root/project/fluid_inference/
 CUDA_LIB=/usr/local/cuda-10.0/lib64/
 CUDNN_LIB=/usr/local/cuda-10.0/lib64/
-OPENCV_DIR=$(pwd)/deps/opencv346/
+OPENCV_DIR=$(pwd)/deps/opencv346_with_contrib/
 echo ${OPENCV_DIR}
 
 rm -rf build
@@ -28,3 +28,4 @@ cmake .. \
     -DWITH_STATIC_LIB=OFF
 make clean
 make -j12
+
