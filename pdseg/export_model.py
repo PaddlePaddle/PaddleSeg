45a8a5af810b5e5a5f1fc5d6cfbd38cfbf4bb506
chenguowei01 <chenguowei01@baidu.com>
2020-05-26 11:58:52 +0800

    fix for use_pr parameter

--- pdseg/export_model.py
+++ pdseg/export_model.py
@@ -53,7 +53,7 @@ def parse_args():
 def export_inference_config():
     deploy_cfg = '''DEPLOY:
         USE_GPU : 1
-        USE_PR : 1
+        USE_PR : 0
         MODEL_PATH : "%s"
         MODEL_FILENAME : "%s"
         PARAMS_FILENAME : "%s"
