00c24b32b10ba8609aefd88e4ed075c501607d9b
chenguowei01 <chenguowei01@baidu.com>
2020-09-14 16:21:02 +0800

    update train.py

--- dygraph/core/train.py
+++ dygraph/core/train.py
@@ -18,7 +18,6 @@ import paddle
 import paddle.fluid as fluid
 from paddle.fluid.dygraph.parallel import ParallelEnv
 from paddle.fluid.io import DataLoader
-# from paddle.incubate.hapi.distributed import DistributedBatchSampler
 from paddle.io import DistributedBatchSampler
 import paddle.nn.functional as F
 
