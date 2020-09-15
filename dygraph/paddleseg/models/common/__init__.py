ea9c7cfdc3261fd99125a3c5c07e75ef1f142895
michaelowenliu <liuyi.ntu@gmail.com>
2020-09-15 17:17:11 +0800

    merge layer_utils and model_utils into layer_libs

--- dygraph/paddleseg/models/common/__init__.py
+++ dygraph/paddleseg/models/common/__init__.py
@@ -13,5 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import layer_utils
-from . import model_utils
+from . import layer_libs
+from . import activation
+from . import pyramid_pool
