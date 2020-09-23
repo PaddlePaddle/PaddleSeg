858077efea3251ebc4ad6a53af299d9f14a87211
michaelowenliu <liuyi.ntu@gmail.com>
2020-09-23 11:21:43 +0800

    fix a typo

--- dygraph/train.py
+++ dygraph/train.py
@@ -19,7 +19,7 @@ from paddle.distributed import ParallelEnv
 
 import paddleseg
 from paddleseg.cvlibs import manager, Config
-from paddleseg.utils import get_environ_info, logger, 
+from paddleseg.utils import get_environ_info, logger
 from paddleseg.core import train
 
 
