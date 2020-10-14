18a086837f86558981707af0ac3f2c96c7f00190
michaelowenliu <liuyi.ntu@gmail.com>
2020-10-14 14:47:46 +0800

    move seg class to front

--- dygraph/paddleseg/models/danet.py
+++ dygraph/paddleseg/models/danet.py
@@ -16,9 +16,101 @@ import paddle
 import paddle.nn as nn
 import paddle.nn.functional as F
 
-from paddleseg.cvlibs import manager, param_init
+from paddleseg.cvlibs import manager
 from paddleseg.models import layers
 from paddleseg.utils import utils
+
+
+@manager.MODELS.add_component
+class DANet(nn.Layer):
+    """
+    The DANet implementation based on PaddlePaddle.
+
+    The original article refers to
+        Fu, jun, et al. "Dual Attention Network for Scene Segmentation"
+        (https://arxiv.org/pdf/1809.02983.pdf)
+
+    Args:
+        num_classes(int): the unique number of target classes.
+        backbone(Paddle.nn.Layer): backbone network.
+        backbone_indices(tuple): values in the tuple indicate the indices of
+                                 output of backbone. Only the last indice is
+                                 used.
+        pretrained(str): the path or url of pretrained model. Default to None.
+    """
+
+    def __init__(self,
+                 num_classes,
+                 backbone,
+                 backbone_indices=None,
+                 pretrained=None):
+        super(DANet, self).__init__()
+
+        self.backbone = backbone
+        self.backbone_indices = backbone_indices
+        in_channels = [self.backbone.feat_channels[i] for i in backbone_indices]
+
+        self.head = DAHead(num_classes=num_classes, in_channels=in_channels)
+
+        utils.load_entire_model(self, pretrained)
+
+    def forward(self, x):
+        feats = self.backbone(x)
+        feats = [feats[i] for i in self.backbone_indices]
+        preds = self.head(feats)
+        preds = [F.resize_bilinear(pred, x.shape[2:]) for pred in preds]
+        return preds
+
+
+class DAHead(nn.Layer):
+    """
+    The Dual attention head.
+
+    Args:
+        num_classes(int): the unique number of target classes.
+        in_channels(tuple): the number of input channels.
+    """
+
+    def __init__(self, num_classes, in_channels=None):
+        super(DAHead, self).__init__()
+        in_channels = in_channels[-1]
+        inter_channels = in_channels // 4
+
+        self.channel_conv = layers.ConvBNReLU(in_channels, inter_channels, 3)
+        self.position_conv = layers.ConvBNReLU(in_channels, inter_channels, 3)
+        self.pam = PAM(inter_channels)
+        self.cam = CAM()
+        self.conv1 = layers.ConvBNReLU(inter_channels, inter_channels, 3)
+        self.conv2 = layers.ConvBNReLU(inter_channels, inter_channels, 3)
+
+        self.aux_head = nn.Sequential(
+            nn.Dropout2d(0.1), nn.Conv2d(in_channels, num_classes, 1))
+
+        self.aux_head_pam = nn.Sequential(
+            nn.Dropout2d(0.1), nn.Conv2d(inter_channels, num_classes, 1))
+
+        self.aux_head_cam = nn.Sequential(
+            nn.Dropout2d(0.1), nn.Conv2d(inter_channels, num_classes, 1))
+
+        self.cls_head = nn.Sequential(
+            nn.Dropout2d(0.1), nn.Conv2d(inter_channels, num_classes, 1))
+
+    def forward(self, feat_list):
+        feats = feat_list[-1]
+        channel_feats = self.channel_conv(feats)
+        channel_feats = self.cam(channel_feats)
+        channel_feats = self.conv1(channel_feats)
+
+        position_feats = self.position_conv(feats)
+        position_feats = self.pam(position_feats)
+        position_feats = self.conv2(position_feats)
+
+        feats_sum = position_feats + channel_feats
+        cam_logit = self.aux_head_cam(channel_feats)
+        pam_logit = self.aux_head_cam(position_feats)
+        logit = self.cls_head(feats_sum)
+        aux_logit = self.aux_head(feats)
+        return [logit, cam_logit, pam_logit, aux_logit]
 
 
 class PAM(nn.Layer):
@@ -98,95 +190,3 @@ class CAM(nn.Layer):
 
         out = self.gamma * feat + x
         return out
-
-
-class DAHead(nn.Layer):
-    """
-    The Dual attention head.
-
-    Args:
-        num_classes(int): the unique number of target classes.
-        in_channels(tuple): the number of input channels.
-    """
-
-    def __init__(self, num_classes, in_channels=None):
-        super(DAHead, self).__init__()
-        in_channels = in_channels[-1]
-        inter_channels = in_channels // 4
-
-        self.channel_conv = layers.ConvBNReLU(in_channels, inter_channels, 3)
-        self.position_conv = layers.ConvBNReLU(in_channels, inter_channels, 3)
-        self.pam = PAM(inter_channels)
-        self.cam = CAM()
-        self.conv1 = layers.ConvBNReLU(inter_channels, inter_channels, 3)
-        self.conv2 = layers.ConvBNReLU(inter_channels, inter_channels, 3)
-
-        self.aux_head = nn.Sequential(
-            nn.Dropout2d(0.1), nn.Conv2d(in_channels, num_classes, 1))
-
-        self.aux_head_pam = nn.Sequential(
-            nn.Dropout2d(0.1), nn.Conv2d(inter_channels, num_classes, 1))
-
-        self.aux_head_cam = nn.Sequential(
-            nn.Dropout2d(0.1), nn.Conv2d(inter_channels, num_classes, 1))
-
-        self.cls_head = nn.Sequential(
-            nn.Dropout2d(0.1), nn.Conv2d(inter_channels, num_classes, 1))
-
-    def forward(self, feat_list):
-        feats = feat_list[-1]
-        channel_feats = self.channel_conv(feats)
-        channel_feats = self.cam(channel_feats)
-        channel_feats = self.conv1(channel_feats)
-
-        position_feats = self.position_conv(feats)
-        position_feats = self.pam(position_feats)
-        position_feats = self.conv2(position_feats)
-
-        feats_sum = position_feats + channel_feats
-        cam_logit = self.aux_head_cam(channel_feats)
-        pam_logit = self.aux_head_cam(position_feats)
-        logit = self.cls_head(feats_sum)
-        aux_logit = self.aux_head(feats)
-        return [logit, cam_logit, pam_logit, aux_logit]
-
-
-@manager.MODELS.add_component
-class DANet(nn.Layer):
-    """
-    The DANet implementation based on PaddlePaddle.
-
-    The original article refers to
-        Fu, jun, et al. "Dual Attention Network for Scene Segmentation"
-        (https://arxiv.org/pdf/1809.02983.pdf)
-
-    Args:
-        num_classes(int): the unique number of target classes.
-        backbone(Paddle.nn.Layer): backbone network.
-        backbone_indices(tuple): values in the tuple indicate the indices of
-                                 output of backbone. Only the last indice is
-                                 used.
-        pretrained(str): the path or url of pretrained model. Default to None.
-    """
-
-    def __init__(self,
-                 num_classes,
-                 backbone,
-                 backbone_indices=None,
-                 pretrained=None):
-        super(DANet, self).__init__()
-
-        self.backbone = backbone
-        self.backbone_indices = backbone_indices
-        in_channels = [self.backbone.feat_channels[i] for i in backbone_indices]
-
-        self.head = DAHead(num_classes=num_classes, in_channels=in_channels)
-
-        utils.load_entire_model(self, pretrained)
-
-    def forward(self, x):
-        feats = self.backbone(x)
-        feats = [feats[i] for i in self.backbone_indices]
-        preds = self.head(feats)
-        preds = [F.resize_bilinear(pred, x.shape[2:]) for pred in preds]
-        return preds
