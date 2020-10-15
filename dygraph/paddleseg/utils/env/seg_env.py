b6d6c5949a045b1fff70f9794f6818d1d418f33f
wuzewu <wuzewu@baidu.com>
2020-10-15 20:43:12 +0800

    Add file comments

--- dygraph/paddleseg/utils/env/seg_env.py
+++ dygraph/paddleseg/utils/env/seg_env.py
@@ -11,6 +11,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""
+This module is used to store environmental parameters in PaddleSeg
+
+SEG_HOME : Root directory for storing PaddleSeg related data. Default to ~/.paddleseg.
+           Users can change the default value through the SEG_HOME environment variable.
+DATA_HOME : The directory to store the automatically downloaded dataset, e.g ADE20K.
+PRETRAINED_MODEL_HOME : The directory to store the automatically downloaded pretrained model.
+"""
 
 import os
 
