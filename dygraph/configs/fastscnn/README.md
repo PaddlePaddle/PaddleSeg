c6a0e903f4592eccf0789c2868b9bb7ea883a84e
wuyefeilin <30919197+wuyefeilin@users.noreply.github.com>
2020-11-13 10:44:02 +0800

    add ms_flip and sliding window  (#576)

--- dygraph/configs/fastscnn/README.md
+++ dygraph/configs/fastscnn/README.md
@@ -8,6 +8,6 @@
 
 ### Cityscapes
 
-| Model | Backbone | Resolution | Training Iters | mIoU | mIoU (multi-scale) | Links |
-|:-:|:-:|:-:|:-:|:-:|:-:|:-:|
-|Fast SCNN|-|1024x1024|160000|69.31%|-|[model](https://bj.bcebos.com/paddleseg/dygraph/cityscapes/fastscnn_cityscapes_1024x1024_160k/model.pdparams) \| [log](https://bj.bcebos.com/paddleseg/dygraph/cityscapes/fastscnn_cityscapes_1024x1024_160k/train.log) \| [vdl](https://paddlepaddle.org.cn/paddle/visualdl/service/app/scalar?id=3b4c3f01c9213cac14e53c69d262a337)|
+| Model | Backbone | Resolution | Training Iters | mIoU | mIoU (flip) | mIoU (ms+flip) | Links |
+|:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|
+|Fast SCNN|-|1024x1024|160000|69.31%|-|-|[model](https://bj.bcebos.com/paddleseg/dygraph/cityscapes/fastscnn_cityscapes_1024x1024_160k/model.pdparams) \| [log](https://bj.bcebos.com/paddleseg/dygraph/cityscapes/fastscnn_cityscapes_1024x1024_160k/train.log) \| [vdl](https://paddlepaddle.org.cn/paddle/visualdl/service/app/scalar?id=3b4c3f01c9213cac14e53c69d262a337)|
--- dygraph/predict.py
+++ dygraph/predict.py
@@ -23,15 +23,15 @@ from paddleseg.core import predict
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Model evaluation')
+    parser = argparse.ArgumentParser(description='Model prediction')
 
-    # params of evaluate
+    # params of prediction
     parser.add_argument(
         "--config", dest="cfg", help="The config file.", default=None, type=str)
     parser.add_argument(
         '--model_path',
         dest='model_path',
-        help='The path of model for evaluation',
+        help='The path of model for prediction',
         type=str,
         default=None)
     parser.add_argument(
@@ -47,6 +47,53 @@ def parse_args():
         help='The directory for saving the predicted results',
         type=str,
         default='./output/result')
+
+    # augment for prediction
+    parser.add_argument(
+        '--aug_pred',
+        dest='aug_pred',
+        help='Whether to use mulit-scales and flip augment for prediction',
+        action='store_true')
+    parser.add_argument(
+        '--scales',
+        dest='scales',
+        nargs='+',
+        help='Scales for augment',
+        type=float,
+        default=1.0)
+    parser.add_argument(
+        '--flip_horizontal',
+        dest='flip_horizontal',
+        help='Whether to use flip horizontally augment',
+        action='store_true')
+    parser.add_argument(
+        '--flip_vertical',
+        dest='flip_vertical',
+        help='Whether to use flip vertically augment',
+        action='store_true')
+
+    # sliding window prediction
+    parser.add_argument(
+        '--is_slide',
+        dest='is_slide',
+        help='Whether to prediction by sliding window',
+        action='store_true')
+    parser.add_argument(
+        '--crop_size',
+        dest='crop_size',
+        nargs=2,
+        help=
+        'The crop size of sliding window, the first is width and the second is height.',
+        type=int,
+        default=None)
+    parser.add_argument(
+        '--stride',
+        dest='stride',
+        nargs=2,
+        help=
+        'The stride of sliding window, the first is width and the second is height.',
+        type=int,
+        default=None)
 
     return parser.parse_args()
 
@@ -109,7 +156,15 @@ def main(args):
         transforms=transforms,
         image_list=image_list,
         image_dir=image_dir,
-        save_dir=args.save_dir)
+        save_dir=args.save_dir,
+        aug_pred=args.aug_pred,
+        scales=args.scales,
+        flip_horizontal=args.flip_horizontal,
+        flip_vertical=args.flip_vertical,
+        is_slide=args.is_slide,
+        crop_size=args.crop_size,
+        stride=args.stride,
+    )
 
 
 if __name__ == '__main__':
