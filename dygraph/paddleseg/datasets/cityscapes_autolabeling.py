3b0064e2589363cc0865852f8adc59ca919806a8
chulutao <chulutao@baidu.com>
2020-12-13 21:03:09 +0800

    modify autolabeling: shuffle every epoch

--- dygraph/paddleseg/datasets/cityscapes_autolabeling.py
+++ dygraph/paddleseg/datasets/cityscapes_autolabeling.py
@@ -68,6 +68,7 @@ class CityscapesAutolabeling(paddle.io.Dataset):
         self.file_list = [[
             img_path, label_path
         ] for img_path, label_path in zip(img_files, label_files)]
+        random.shuffle(self.file_list)
 
         if mode == 'train':
 
@@ -95,6 +96,10 @@ class CityscapesAutolabeling(paddle.io.Dataset):
             self.coarse_file_list = [[img_path, label_path]
                                      for img_path, label_path in zip(
                                          coarse_img_files, coarse_label_files)]
+            random.shuffle(self.coarse_file_list)
+            self.num_coarse = len(self.coarse_file_list)
+            self.rank = 0
+            self.coarse_rank = 0
 
         # Keep the same number of files in one epoch even using coarse data.
         self.num_files = len(self.file_list)
@@ -112,12 +117,21 @@ class CityscapesAutolabeling(paddle.io.Dataset):
             return im, label
         else:
             if idx / self.num_files < self.coarse_proba:
-                rand_idx = np.random.randint(0, len(self.coarse_file_list))
-                image_path, label_path = self.coarse_file_list[rand_idx]
+                #                 rand_idx = np.random.randint(0, len(self.coarse_file_list))
+                #                 image_path, label_path = self.coarse_file_list[rand_idx]
+                image_path, label_path = self.coarse_file_list[self.coarse_rank]
+                self.coarse_rank += 1
+                if self.coarse_rank >= self.num_coarse:
+                    self.coarse_rank = 0
+                    random.shuffle(self.coarse_file_list)
             else:
-                rand_idx = np.random.randint(0, len(self.file_list))
-                image_path, label_path = self.file_list[rand_idx]
-
+                #                 rand_idx = np.random.randint(0, len(self.file_list))
+                #                 image_path, label_path = self.file_list[rand_idx]
+                image_path, label_path = self.file_list[self.rank]
+                self.rank += 1
+                if self.rank >= self.num_files:
+                    self.rank = 0
+                    random.shuffle(self.file_list)
             im, label = self.transforms(im=image_path, label=label_path)
             return im, label
 
