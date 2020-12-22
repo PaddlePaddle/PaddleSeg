3663d4a6bf8ea37118689ed29c2ef273704e0930
LutaoChu <30695251+LutaoChu@users.noreply.github.com>
2020-12-22 16:13:28 +0800

    add dice loss, edge_attention_loss, gscnn_dual_task_loss (#723)

--- dygraph/paddleseg/models/losses/__init__.py
+++ dygraph/paddleseg/models/losses/__init__.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .cross_entroy_loss import CrossEntropyLoss
-
+from .cross_entropy_loss import CrossEntropyLoss
+from .binary_cross_entropy_loss import BCELoss
+from .gscnn_dual_task_loss import DualTaskLoss
+from .edge_attention_loss import EdgeAttentionLoss
 from .bootstrapped_cross_entropy import BootstrappedCrossEntropyLoss
+from .dice_loss import DiceLoss
