b79884917b15a145ebc0aee86f67288129ccf00b
Liu Yi <liuyi.ntu@gmail.com>
2021-01-04 10:05:45 +0800

    Add License

--- paddleseg/models/u2net.py
+++ paddleseg/models/u2net.py
@@ -1,11 +1,264 @@
+# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import paddle
 import paddle.nn as nn
 import paddle.nn.functional as F
-from paddleseg import utils
+
 from paddleseg.cvlibs import manager
+from paddleseg.models import layers
+from paddleseg.utils import utils
 
 __all__ = ['U2Net', 'U2Netp']
 
+@manager.MODELS.add_component
+class U2Net(nn.Layer):
+    """
+    The U^2-Net implementation based on PaddlePaddle.
+
+    The original article refers to
+    Xuebin Qin, et, al. "U^2-Net: Going Deeper with Nested U-Structure for Salient Object Detection"
+    (https://arxiv.org/abs/2005.09007).
+
+    Args:
+        num_classes (int): The unique number of target classes.
+        in_ch (int, optional): Input channels. Default: 3.
+        pretrained (str, optional): The path or url of pretrained model for fine tuning. Default: None.
+
+    """
+
+    def __init__(self, num_classes, in_ch=3, pretrained=None):
+        super(U2Net, self).__init__()
+
+        self.stage1 = RSU7(in_ch, 32, 64)
+        self.pool12 = nn.MaxPool2D(2, stride=2, ceil_mode=True)
+
+        self.stage2 = RSU6(64, 32, 128)
+        self.pool23 = nn.MaxPool2D(2, stride=2, ceil_mode=True)
+
+        self.stage3 = RSU5(128, 64, 256)
+        self.pool34 = nn.MaxPool2D(2, stride=2, ceil_mode=True)
+
+        self.stage4 = RSU4(256, 128, 512)
+        self.pool45 = nn.MaxPool2D(2, stride=2, ceil_mode=True)
+
+        self.stage5 = RSU4F(512, 256, 512)
+        self.pool56 = nn.MaxPool2D(2, stride=2, ceil_mode=True)
+
+        self.stage6 = RSU4F(512, 256, 512)
+
+        # decoder
+        self.stage5d = RSU4F(1024, 256, 512)
+        self.stage4d = RSU4(1024, 128, 256)
+        self.stage3d = RSU5(512, 64, 128)
+        self.stage2d = RSU6(256, 32, 64)
+        self.stage1d = RSU7(128, 16, 64)
+
+        self.side1 = nn.Conv2D(64, num_classes, 3, padding=1)
+        self.side2 = nn.Conv2D(64, num_classes, 3, padding=1)
+        self.side3 = nn.Conv2D(128, num_classes, 3, padding=1)
+        self.side4 = nn.Conv2D(256, num_classes, 3, padding=1)
+        self.side5 = nn.Conv2D(512, num_classes, 3, padding=1)
+        self.side6 = nn.Conv2D(512, num_classes, 3, padding=1)
+
+        self.outconv = nn.Conv2D(6 * num_classes, num_classes, 1)
+
+        self.pretrained = pretrained
+        self.init_weight()
+
+    def forward(self, x):
+
+        hx = x
+
+        #stage 1
+        hx1 = self.stage1(hx)
+        hx = self.pool12(hx1)
+
+        #stage 2
+        hx2 = self.stage2(hx)
+        hx = self.pool23(hx2)
+
+        #stage 3
+        hx3 = self.stage3(hx)
+        hx = self.pool34(hx3)
+
+        #stage 4
+        hx4 = self.stage4(hx)
+        hx = self.pool45(hx4)
+
+        #stage 5
+        hx5 = self.stage5(hx)
+        hx = self.pool56(hx5)
+
+        #stage 6
+        hx6 = self.stage6(hx)
+        hx6up = _upsample_like(hx6, hx5)
+
+        #-------------------- decoder --------------------
+        hx5d = self.stage5d(paddle.concat((hx6up, hx5), 1))
+        hx5dup = _upsample_like(hx5d, hx4)
+
+        hx4d = self.stage4d(paddle.concat((hx5dup, hx4), 1))
+        hx4dup = _upsample_like(hx4d, hx3)
+
+        hx3d = self.stage3d(paddle.concat((hx4dup, hx3), 1))
+        hx3dup = _upsample_like(hx3d, hx2)
+
+        hx2d = self.stage2d(paddle.concat((hx3dup, hx2), 1))
+        hx2dup = _upsample_like(hx2d, hx1)
+
+        hx1d = self.stage1d(paddle.concat((hx2dup, hx1), 1))
+
+        #side output
+        d1 = self.side1(hx1d)
+
+        d2 = self.side2(hx2d)
+        d2 = _upsample_like(d2, d1)
+
+        d3 = self.side3(hx3d)
+        d3 = _upsample_like(d3, d1)
+
+        d4 = self.side4(hx4d)
+        d4 = _upsample_like(d4, d1)
+
+        d5 = self.side5(hx5d)
+        d5 = _upsample_like(d5, d1)
+
+        d6 = self.side6(hx6)
+        d6 = _upsample_like(d6, d1)
+
+        d0 = self.outconv(paddle.concat((d1, d2, d3, d4, d5, d6), 1))
+
+        return [d0, d1, d2, d3, d4, d5, d6]
+
+    def init_weight(self):
+        if self.pretrained is not None:
+            utils.load_entire_model(self, self.pretrained)
+
+
+### U^2-Net small ###
+@manager.MODELS.add_component
+class U2Netp(nn.Layer):
+    """Please Refer to U2Net above."""
+
+    def __init__(self, num_classes, in_ch=3, pretrained=None):
+        super(U2Netp, self).__init__()
+
+        self.stage1 = RSU7(in_ch, 16, 64)
+        self.pool12 = nn.MaxPool2D(2, stride=2, ceil_mode=True)
+
+        self.stage2 = RSU6(64, 16, 64)
+        self.pool23 = nn.MaxPool2D(2, stride=2, ceil_mode=True)
+
+        self.stage3 = RSU5(64, 16, 64)
+        self.pool34 = nn.MaxPool2D(2, stride=2, ceil_mode=True)
+
+        self.stage4 = RSU4(64, 16, 64)
+        self.pool45 = nn.MaxPool2D(2, stride=2, ceil_mode=True)
+
+        self.stage5 = RSU4F(64, 16, 64)
+        self.pool56 = nn.MaxPool2D(2, stride=2, ceil_mode=True)
+
+        self.stage6 = RSU4F(64, 16, 64)
+
+        # decoder
+        self.stage5d = RSU4F(128, 16, 64)
+        self.stage4d = RSU4(128, 16, 64)
+        self.stage3d = RSU5(128, 16, 64)
+        self.stage2d = RSU6(128, 16, 64)
+        self.stage1d = RSU7(128, 16, 64)
+
+        self.side1 = nn.Conv2D(64, num_classes, 3, padding=1)
+        self.side2 = nn.Conv2D(64, num_classes, 3, padding=1)
+        self.side3 = nn.Conv2D(64, num_classes, 3, padding=1)
+        self.side4 = nn.Conv2D(64, num_classes, 3, padding=1)
+        self.side5 = nn.Conv2D(64, num_classes, 3, padding=1)
+        self.side6 = nn.Conv2D(64, num_classes, 3, padding=1)
+
+        self.outconv = nn.Conv2D(6 * num_classes, num_classes, 1)
+
+        self.pretrained = pretrained
+        self.init_weight()
+
+    def forward(self, x):
+
+        hx = x
+
+        #stage 1
+        hx1 = self.stage1(hx)
+        hx = self.pool12(hx1)
+
+        #stage 2
+        hx2 = self.stage2(hx)
+        hx = self.pool23(hx2)
+
+        #stage 3
+        hx3 = self.stage3(hx)
+        hx = self.pool34(hx3)
+
+        #stage 4
+        hx4 = self.stage4(hx)
+        hx = self.pool45(hx4)
+
+        #stage 5
+        hx5 = self.stage5(hx)
+        hx = self.pool56(hx5)
+
+        #stage 6
+        hx6 = self.stage6(hx)
+        hx6up = _upsample_like(hx6, hx5)
+
+        #decoder
+        hx5d = self.stage5d(paddle.concat((hx6up, hx5), 1))
+        hx5dup = _upsample_like(hx5d, hx4)
+
+        hx4d = self.stage4d(paddle.concat((hx5dup, hx4), 1))
+        hx4dup = _upsample_like(hx4d, hx3)
+
+        hx3d = self.stage3d(paddle.concat((hx4dup, hx3), 1))
+        hx3dup = _upsample_like(hx3d, hx2)
+
+        hx2d = self.stage2d(paddle.concat((hx3dup, hx2), 1))
+        hx2dup = _upsample_like(hx2d, hx1)
+
+        hx1d = self.stage1d(paddle.concat((hx2dup, hx1), 1))
+
+        #side output
+        d1 = self.side1(hx1d)
+
+        d2 = self.side2(hx2d)
+        d2 = _upsample_like(d2, d1)
+
+        d3 = self.side3(hx3d)
+        d3 = _upsample_like(d3, d1)
+
+        d4 = self.side4(hx4d)
+        d4 = _upsample_like(d4, d1)
+
+        d5 = self.side5(hx5d)
+        d5 = _upsample_like(d5, d1)
+
+        d6 = self.side6(hx6)
+        d6 = _upsample_like(d6, d1)
+
+        d0 = self.outconv(paddle.concat((d1, d2, d3, d4, d5, d6), 1))
+
+        return [d0, d1, d2, d3, d4, d5, d6]
+
+    def init_weight(self):
+        if self.pretrained is not None:
+            utils.load_entire_model(self, self.pretrained)
 
 class REBNCONV(nn.Layer):
     def __init__(self, in_ch=3, out_ch=3, dirate=1):
@@ -317,243 +570,3 @@ class RSU4F(nn.Layer):  #UNet04FRES(nn.Layer):
         hx1d = self.rebnconv1d(paddle.concat((hx2d, hx1), 1))
 
         return hx1d + hxin
-
-
-##### U^2-Net ####
-@manager.MODELS.add_component
-class U2Net(nn.Layer):
-    """
-    The U^2-Net implementation based on PaddlePaddle.
-
-    The original article refers to
-    Xuebin Qin, et, al. "U^2-Net: Going Deeper with Nested U-Structure for Salient Object Detection"
-    (https://arxiv.org/abs/2005.09007).
-
-    Args:
-        num_classes (int): The unique number of target classes.
-        in_ch (int, optional): Input channels. Default: 3.
-        pretrained (str, optional): The path or url of pretrained model for fine tuning. Default: None.
-
-    """
-
-    def __init__(self, num_classes, in_ch=3, pretrained=None):
-        super(U2Net, self).__init__()
-
-        self.stage1 = RSU7(in_ch, 32, 64)
-        self.pool12 = nn.MaxPool2D(2, stride=2, ceil_mode=True)
-
-        self.stage2 = RSU6(64, 32, 128)
-        self.pool23 = nn.MaxPool2D(2, stride=2, ceil_mode=True)
-
-        self.stage3 = RSU5(128, 64, 256)
-        self.pool34 = nn.MaxPool2D(2, stride=2, ceil_mode=True)
-
-        self.stage4 = RSU4(256, 128, 512)
-        self.pool45 = nn.MaxPool2D(2, stride=2, ceil_mode=True)
-
-        self.stage5 = RSU4F(512, 256, 512)
-        self.pool56 = nn.MaxPool2D(2, stride=2, ceil_mode=True)
-
-        self.stage6 = RSU4F(512, 256, 512)
-
-        # decoder
-        self.stage5d = RSU4F(1024, 256, 512)
-        self.stage4d = RSU4(1024, 128, 256)
-        self.stage3d = RSU5(512, 64, 128)
-        self.stage2d = RSU6(256, 32, 64)
-        self.stage1d = RSU7(128, 16, 64)
-
-        self.side1 = nn.Conv2D(64, num_classes, 3, padding=1)
-        self.side2 = nn.Conv2D(64, num_classes, 3, padding=1)
-        self.side3 = nn.Conv2D(128, num_classes, 3, padding=1)
-        self.side4 = nn.Conv2D(256, num_classes, 3, padding=1)
-        self.side5 = nn.Conv2D(512, num_classes, 3, padding=1)
-        self.side6 = nn.Conv2D(512, num_classes, 3, padding=1)
-
-        self.outconv = nn.Conv2D(6 * num_classes, num_classes, 1)
-
-        self.pretrained = pretrained
-        self.init_weight()
-
-    def forward(self, x):
-
-        hx = x
-
-        #stage 1
-        hx1 = self.stage1(hx)
-        hx = self.pool12(hx1)
-
-        #stage 2
-        hx2 = self.stage2(hx)
-        hx = self.pool23(hx2)
-
-        #stage 3
-        hx3 = self.stage3(hx)
-        hx = self.pool34(hx3)
-
-        #stage 4
-        hx4 = self.stage4(hx)
-        hx = self.pool45(hx4)
-
-        #stage 5
-        hx5 = self.stage5(hx)
-        hx = self.pool56(hx5)
-
-        #stage 6
-        hx6 = self.stage6(hx)
-        hx6up = _upsample_like(hx6, hx5)
-
-        #-------------------- decoder --------------------
-        hx5d = self.stage5d(paddle.concat((hx6up, hx5), 1))
-        hx5dup = _upsample_like(hx5d, hx4)
-
-        hx4d = self.stage4d(paddle.concat((hx5dup, hx4), 1))
-        hx4dup = _upsample_like(hx4d, hx3)
-
-        hx3d = self.stage3d(paddle.concat((hx4dup, hx3), 1))
-        hx3dup = _upsample_like(hx3d, hx2)
-
-        hx2d = self.stage2d(paddle.concat((hx3dup, hx2), 1))
-        hx2dup = _upsample_like(hx2d, hx1)
-
-        hx1d = self.stage1d(paddle.concat((hx2dup, hx1), 1))
-
-        #side output
-        d1 = self.side1(hx1d)
-
-        d2 = self.side2(hx2d)
-        d2 = _upsample_like(d2, d1)
-
-        d3 = self.side3(hx3d)
-        d3 = _upsample_like(d3, d1)
-
-        d4 = self.side4(hx4d)
-        d4 = _upsample_like(d4, d1)
-
-        d5 = self.side5(hx5d)
-        d5 = _upsample_like(d5, d1)
-
-        d6 = self.side6(hx6)
-        d6 = _upsample_like(d6, d1)
-
-        d0 = self.outconv(paddle.concat((d1, d2, d3, d4, d5, d6), 1))
-
-        return [d0, d1, d2, d3, d4, d5, d6]
-
-    def init_weight(self):
-        if self.pretrained is not None:
-            utils.load_entire_model(self, self.pretrained)
-
-
-### U^2-Net small ###
-@manager.MODELS.add_component
-class U2Netp(nn.Layer):
-    """Please Refer to U2Net above."""
-
-    def __init__(self, num_classes, in_ch=3, pretrained=None):
-        super(U2Netp, self).__init__()
-
-        self.stage1 = RSU7(in_ch, 16, 64)
-        self.pool12 = nn.MaxPool2D(2, stride=2, ceil_mode=True)
-
-        self.stage2 = RSU6(64, 16, 64)
-        self.pool23 = nn.MaxPool2D(2, stride=2, ceil_mode=True)
-
-        self.stage3 = RSU5(64, 16, 64)
-        self.pool34 = nn.MaxPool2D(2, stride=2, ceil_mode=True)
-
-        self.stage4 = RSU4(64, 16, 64)
-        self.pool45 = nn.MaxPool2D(2, stride=2, ceil_mode=True)
-
-        self.stage5 = RSU4F(64, 16, 64)
-        self.pool56 = nn.MaxPool2D(2, stride=2, ceil_mode=True)
-
-        self.stage6 = RSU4F(64, 16, 64)
-
-        # decoder
-        self.stage5d = RSU4F(128, 16, 64)
-        self.stage4d = RSU4(128, 16, 64)
-        self.stage3d = RSU5(128, 16, 64)
-        self.stage2d = RSU6(128, 16, 64)
-        self.stage1d = RSU7(128, 16, 64)
-
-        self.side1 = nn.Conv2D(64, num_classes, 3, padding=1)
-        self.side2 = nn.Conv2D(64, num_classes, 3, padding=1)
-        self.side3 = nn.Conv2D(64, num_classes, 3, padding=1)
-        self.side4 = nn.Conv2D(64, num_classes, 3, padding=1)
-        self.side5 = nn.Conv2D(64, num_classes, 3, padding=1)
-        self.side6 = nn.Conv2D(64, num_classes, 3, padding=1)
-
-        self.outconv = nn.Conv2D(6 * num_classes, num_classes, 1)
-
-        self.pretrained = pretrained
-        self.init_weight()
-
-    def forward(self, x):
-
-        hx = x
-
-        #stage 1
-        hx1 = self.stage1(hx)
-        hx = self.pool12(hx1)
-
-        #stage 2
-        hx2 = self.stage2(hx)
-        hx = self.pool23(hx2)
-
-        #stage 3
-        hx3 = self.stage3(hx)
-        hx = self.pool34(hx3)
-
-        #stage 4
-        hx4 = self.stage4(hx)
-        hx = self.pool45(hx4)
-
-        #stage 5
-        hx5 = self.stage5(hx)
-        hx = self.pool56(hx5)
-
-        #stage 6
-        hx6 = self.stage6(hx)
-        hx6up = _upsample_like(hx6, hx5)
-
-        #decoder
-        hx5d = self.stage5d(paddle.concat((hx6up, hx5), 1))
-        hx5dup = _upsample_like(hx5d, hx4)
-
-        hx4d = self.stage4d(paddle.concat((hx5dup, hx4), 1))
-        hx4dup = _upsample_like(hx4d, hx3)
-
-        hx3d = self.stage3d(paddle.concat((hx4dup, hx3), 1))
-        hx3dup = _upsample_like(hx3d, hx2)
-
-        hx2d = self.stage2d(paddle.concat((hx3dup, hx2), 1))
-        hx2dup = _upsample_like(hx2d, hx1)
-
-        hx1d = self.stage1d(paddle.concat((hx2dup, hx1), 1))
-
-        #side output
-        d1 = self.side1(hx1d)
-
-        d2 = self.side2(hx2d)
-        d2 = _upsample_like(d2, d1)
-
-        d3 = self.side3(hx3d)
-        d3 = _upsample_like(d3, d1)
-
-        d4 = self.side4(hx4d)
-        d4 = _upsample_like(d4, d1)
-
-        d5 = self.side5(hx5d)
-        d5 = _upsample_like(d5, d1)
-
-        d6 = self.side6(hx6)
-        d6 = _upsample_like(d6, d1)
-
-        d0 = self.outconv(paddle.concat((d1, d2, d3, d4, d5, d6), 1))
-
-        return [d0, d1, d2, d3, d4, d5, d6]
-
-    def init_weight(self):
-        if self.pretrained is not None:
-            utils.load_entire_model(self, self.pretrained)
