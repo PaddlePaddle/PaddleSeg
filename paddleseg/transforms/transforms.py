245f1229e4d37614fdbe001f4c2be2d37defe8fe
LutaoChu <30695251+LutaoChu@users.noreply.github.com>
2021-05-11 11:57:31 +0800

    Fix transforms bug.

--- paddleseg/transforms/transforms.py
+++ paddleseg/transforms/transforms.py
@@ -718,13 +718,14 @@ class RandomRotation:
                 flags=cv2.INTER_LINEAR,
                 borderMode=cv2.BORDER_CONSTANT,
                 borderValue=self.im_padding_value)
-            label = cv2.warpAffine(
-                label,
-                r,
-                dsize=dsize,
-                flags=cv2.INTER_NEAREST,
-                borderMode=cv2.BORDER_CONSTANT,
-                borderValue=self.label_padding_value)
+            if label is not None:
+                label = cv2.warpAffine(
+                    label,
+                    r,
+                    dsize=dsize,
+                    flags=cv2.INTER_NEAREST,
+                    borderMode=cv2.BORDER_CONSTANT,
+                    borderValue=self.label_padding_value)
 
         if label is None:
             return (im, )
@@ -778,13 +779,14 @@ class RandomScaleAspect:
                     w1 = np.random.randint(0, img_width - dw)
 
                     im = im[h1:(h1 + dh), w1:(w1 + dw), :]
-                    label = label[h1:(h1 + dh), w1:(w1 + dw)]
                     im = cv2.resize(
                         im, (img_width, img_height),
                         interpolation=cv2.INTER_LINEAR)
-                    label = cv2.resize(
-                        label, (img_width, img_height),
-                        interpolation=cv2.INTER_NEAREST)
+                    if label is not None:
+                        label = label[h1:(h1 + dh), w1:(w1 + dw)]
+                        label = cv2.resize(
+                            label, (img_width, img_height),
+                            interpolation=cv2.INTER_NEAREST)
                     break
         if label is None:
             return (im, )
