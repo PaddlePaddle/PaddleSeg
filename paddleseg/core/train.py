9e643f6b2b58d56d001192d3d21324d1ef6e9e0a
chulutao <chulutao@baidu.com>
2021-05-19 14:43:32 +0800

    modify core/train.py

--- paddleseg/core/train.py
+++ paddleseg/core/train.py
@@ -204,7 +204,7 @@ def train(model,
             if (iter % save_interval == 0
                     or iter == iters) and (val_dataset is not None):
                 num_workers = 1 if num_workers > 0 else 0
-                mean_iou, acc = evaluate(
+                mean_iou, acc, _, _, _ = evaluate(
                     model, val_dataset, num_workers=num_workers)
                 model.train()
 
