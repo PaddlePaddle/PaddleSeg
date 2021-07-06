22256e6707d88a5fa7625f681ae134fcabd95d30
geoyee <geoyee@yeah.net>
2021-07-06 14:26:07 +0800

    add eiseg

---
+++ contrib/EISeg/eiseg/inference/transforms/__init__.py
@@ -0,0 +1,5 @@
+from .base import SigmoidForPred
+from .flip import AddHorizontalFlip
+from .zoom_in import ZoomIn
+from .limit_longest_side import LimitLongestSide
+from .crops import Crops
---
+++ contrib/EISeg/eiseg/util/__init__.py
@@ -0,0 +1,5 @@
+from .qt import newAction, addActions, struct, newIcon
+from .label import saveLabel, readLabel
+from .config import parseConfigs, saveConfigs
+from .network import model_path
+from .colormap import ColorMask
