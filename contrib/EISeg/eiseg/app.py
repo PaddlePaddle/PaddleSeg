a52c823abc08a84e092159aa9931f22615be747c
geoyee <geoyee@yeah.net>
2021-08-18 16:45:40 +0800

    fix create polygon without mask error

--- contrib/EISeg/eiseg/app.py
+++ contrib/EISeg/eiseg/app.py
@@ -1055,35 +1055,36 @@ class APP_EISeg(QMainWindow, Ui_EISeg):
         if not self.controller or self.image is None:
             return
         current_mask, curr_polygon = self.controller.finishObject()
-        self.updateImage()
-        if current_mask is not None:
-            color = self.controller.labelList[self.currLabelIdx].color
-            for points in curr_polygon:
-                if len(points) < 3:
-                    continue
-                poly = PolygonAnnotation(
-                    self.controller.labelList[self.currLabelIdx].idx,
-                    self.controller.image.shape,
-                    self.delPolygon,
-                    color,
-                    color,
-                    self.opacity,
-                )
-                poly.labelIndex = self.controller.labelList[self.currLabelIdx].idx
-                self.scene.addItem(poly)
-                self.scene.polygon_items.append(poly)
-                for p in points:
-                    poly.addPointLast(QtCore.QPointF(p[0], p[1]))
-                self.setDirty()
-        if self.status == self.EDITING:
-            self.anning = True
-            for p in self.scene.polygon_items:
-                p.setAnning(isAnning=True)
-        else:
-            self.anning = False
-            for p in self.scene.polygon_items:
-                p.setAnning(isAnning=False)
-        self.getMask()
+        if curr_polygon is not None:
+            self.updateImage()
+            if current_mask is not None:
+                color = self.controller.labelList[self.currLabelIdx].color
+                for points in curr_polygon:
+                    if len(points) < 3:
+                        continue
+                    poly = PolygonAnnotation(
+                        self.controller.labelList[self.currLabelIdx].idx,
+                        self.controller.image.shape,
+                        self.delPolygon,
+                        color,
+                        color,
+                        self.opacity,
+                    )
+                    poly.labelIndex = self.controller.labelList[self.currLabelIdx].idx
+                    self.scene.addItem(poly)
+                    self.scene.polygon_items.append(poly)
+                    for p in points:
+                        poly.addPointLast(QtCore.QPointF(p[0], p[1]))
+                    self.setDirty()
+            if self.status == self.EDITING:
+                self.anning = True
+                for p in self.scene.polygon_items:
+                    p.setAnning(isAnning=True)
+            else:
+                self.anning = False
+                for p in self.scene.polygon_items:
+                    p.setAnning(isAnning=False)
+            self.getMask()
 
     def completeLastMask(self):
         # 返回最后一个标签是否完成，false就是还有带点的
--- contrib/EISeg/eiseg/util/polygon.py
+++ contrib/EISeg/eiseg/util/polygon.py
@@ -16,34 +16,38 @@ def get_polygon(label, sample=2):
     cv2_v = cv2.__version__.split(".")[0]
     contours = results[1] if cv2_v == "3" else results[0]  # 边界
     hierarchys = results[2] if cv2_v == "3" else results[1]  # 隶属信息
-    polygons = []
-    relas = []
-    for contour, hierarchy in zip(contours, hierarchys[0]):
-        out = cv2.approxPolyDP(contour, sample, True)
-        # 判断自己，如果是子对象就不管自己是谁
-        if hierarchy[2] == -1:
-            own = None
-        else:
-            if hierarchy[0] == -1 and hierarchy[1] == -1:
-                own = 0
-            elif hierarchy[0] != -1 and hierarchy[1] == -1:
-                own = hierarchy[0] - 1
+    if len(contours) != 0:  # 可能出现没有边界的情况
+        polygons = []
+        relas = []
+        for contour, hierarchy in zip(contours, hierarchys[0]):
+            out = cv2.approxPolyDP(contour, sample, True)
+            # 判断自己，如果是子对象就不管自己是谁
+            if hierarchy[2] == -1:
+                own = None
             else:
-                own = hierarchy[1] + 1
-        rela = (own,  # own
-                hierarchy[-1] if hierarchy[-1] != -1 else None)  # parent
-        polygon = []
-        for p in out:
-            polygon.append(p[0])
-        polygons.append(polygon)  # 边界
-        relas.append(rela)  # 关系
-    for i in range(len(relas)):
-        if relas[i][1] != None:  # 有父母
-            for j in range(len(relas)):
-                if relas[j][0] == relas[i][1]:  # i的父母就是j（i是j的内圈）
-                    polygons[j].append(polygons[j][0])  # 闭合
-                    polygons[j].extend(polygons[i])
-                    polygons[j].append(polygons[i][0])  # 闭合
-                    polygons[i] = None
-    polygons = list(filter(None, polygons))  # 清除加到外圈的内圈多边形
-    return polygons
+                if hierarchy[0] == -1 and hierarchy[1] == -1:
+                    own = 0
+                elif hierarchy[0] != -1 and hierarchy[1] == -1:
+                    own = hierarchy[0] - 1
+                else:
+                    own = hierarchy[1] + 1
+            rela = (own,  # own
+                    hierarchy[-1] if hierarchy[-1] != -1 else None)  # parent
+            polygon = []
+            for p in out:
+                polygon.append(p[0])
+            polygons.append(polygon)  # 边界
+            relas.append(rela)  # 关系
+        for i in range(len(relas)):
+            if relas[i][1] != None:  # 有父母
+                for j in range(len(relas)):
+                    if relas[j][0] == relas[i][1]:  # i的父母就是j（i是j的内圈）
+                        polygons[j].append(polygons[j][0])  # 闭合
+                        polygons[j].extend(polygons[i])
+                        polygons[j].append(polygons[i][0])  # 闭合
+                        polygons[i] = None
+        polygons = list(filter(None, polygons))  # 清除加到外圈的内圈多边形
+        return polygons
+    else:
+        print("没有标签范围，无法生成边界")
+        return None
