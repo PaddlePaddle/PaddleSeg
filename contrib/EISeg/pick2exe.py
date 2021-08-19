69b0e42fc516ed385fc5735800ee31fc34e94793
geoyee <geoyee@yeah.net>
2021-08-19 22:50:02 +0800

    add EISeg download link

--- contrib/EISeg/pick2exe.py
+++ contrib/EISeg/pick2exe.py
@@ -1,8 +1,8 @@
 from qpt.executor import CreateExecutableModule as CEM
 
-module = CEM(work_dir="eiseg",
-             launcher_py_path="eiseg/exe.py",
-             save_path="out")
+module = CEM(work_dir=r"E:\dataFiles\github\PaddleSeg\contrib\EISeg\eiseg",
+             launcher_py_path=r"E:\dataFiles\github\PaddleSeg\contrib\EISeg\eiseg\exe.py",
+             save_path=r"E:\dataFiles\github\PaddleSeg\contrib\EISeg\out")
 
 # 开始打包
 module.make()
