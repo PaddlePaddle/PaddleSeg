3035a7a8a061d7e814a8f850007948eb50498af1
geoyee <geoyee@yeah.net>
2021-08-20 11:45:33 +0800

    del pick2exe

--- contrib/EISeg/pick2exe.py
+++
@@ -1,8 +0,0 @@
-from qpt.executor import CreateExecutableModule as CEM
-
-module = CEM(work_dir=r"E:\dataFiles\github\PaddleSeg\contrib\EISeg\eiseg",
-             launcher_py_path=r"E:\dataFiles\github\PaddleSeg\contrib\EISeg\eiseg\exe.py",
-             save_path=r"E:\dataFiles\github\PaddleSeg\contrib\EISeg\out")
-
-# 开始打包
-module.make()
