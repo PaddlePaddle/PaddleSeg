e62d88a6667d4cb6f4cf8d329714d3e1f143fb50
LutaoChu <30695251+LutaoChu@users.noreply.github.com>
2021-08-23 20:16:50 +0800

    Enhance Python deployment capabilities (#1230)

--- paddleseg/utils/utils.py
+++ paddleseg/utils/utils.py
@@ -123,3 +123,40 @@ def resume(model, optimizer, resume_model):
 
 def worker_init_fn(worker_id):
     np.random.seed(random.randint(0, 100000))
+
+
+def get_image_list(image_path):
+    """Get image list"""
+    valid_suffix = [
+        '.JPEG', '.jpeg', '.JPG', '.jpg', '.BMP', '.bmp', '.PNG', '.png'
+    ]
+    image_list = []
+    image_dir = None
+    if os.path.isfile(image_path):
+        if os.path.splitext(image_path)[-1] in valid_suffix:
+            image_list.append(image_path)
+        else:
+            image_dir = os.path.dirname(image_path)
+            with open(image_path, 'r') as f:
+                for line in f:
+                    line = line.strip()
+                    if len(line.split()) > 1:
+                        line = line.split()[0]
+                    image_list.append(os.path.join(image_dir, line))
+    elif os.path.isdir(image_path):
+        image_dir = image_path
+        for root, dirs, files in os.walk(image_path):
+            for f in files:
+                if '.ipynb_checkpoints' in root:
+                    continue
+                if os.path.splitext(f)[-1] in valid_suffix:
+                    image_list.append(os.path.join(root, f))
+    else:
+        raise FileNotFoundError(
+            '`--image_path` is not found. it should be an image file or a directory including images'
+        )
+
+    if len(image_list) == 0:
+        raise RuntimeError('There are not image file in `--image_path`')
+
+    return image_list, image_dir
