db273ab6f9838ac38d735145d271008adf10f711
xbchen <44107003+CuberrChen@users.noreply.github.com>
2021-09-10 20:46:10 +0800

    add PointRend

--- paddleseg/models/losses/__init__.py
+++ paddleseg/models/losses/__init__.py
@@ -29,5 +29,4 @@ from .mean_square_error_loss import MSELoss
 from .focal_loss import FocalLoss
 from .kl_loss import KLLoss
 from .rmi_loss import RMILoss
-from .detail_aggregate_loss import DetailAggregateLoss
 from .point_cross_entropy_loss import PointCrossEntropyLoss
