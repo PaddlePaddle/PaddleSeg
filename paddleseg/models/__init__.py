1233167e694b50a1be42dc1a6319b35973026559
ETTR123 <740580207@qq.com>
2021-12-21 09:11:12 +0800

    Update __init__.py

--- paddleseg/models/__init__.py
+++ paddleseg/models/__init__.py
@@ -48,4 +48,5 @@ from .segmenter import *
 from .segnet import SegNet
 from .hrnet_contrast import HRNetW48Contrast
 from .espnet import ESPNetV2
+from .dmnet import DMNet
 from .pfpnnet import PFPNNet
