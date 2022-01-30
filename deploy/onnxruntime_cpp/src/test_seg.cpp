6e3851fa7ebed445cc4a914fb94a74e2927f0211
xmba15 <thba1590@gmail.com>
2022-01-30 11:46:43 +0900

    Remove unnecessary variables

--- deploy/onnxruntime_cpp/src/test_seg.cpp
+++ deploy/onnxruntime_cpp/src/test_seg.cpp
@@ -35,40 +35,11 @@ class OrtSessionHandler {
 
 constexpr int BISENETV2_CITYSCAPES_IMAGE_HEIGHT = 1024;
 constexpr int BISENETV2_CITYSCAPES_IMAGE_WIDTH = 1024;
-constexpr int CITYSCAPES_NUM_CLASSES = 19;
 
-static const std::vector<std::string> CITY_SCAPES_CLASSES = {
-    "road", "sidewalk", "building", "wall", "fence", "pole", "traffic light", "traffic sign", "vegetation", "terrain",
-    "sky",  "person",   "rider",    "car",  "truck", "bus",  "train",         "motorcycle",   "bicycle"};
-
-inline std::vector<cv::Scalar> to_cv_sccalar_colors(const std::vector<std::array<int, 3>> &colors) {
-  std::vector<cv::Scalar> result;
-  result.reserve(colors.size());
-  std::transform(std::begin(colors), std::end(colors), std::back_inserter(result),
-                 [](const auto &elem) { return cv::Scalar(elem[0], elem[1], elem[2]); });
-
-  return result;
-}
-
-static const std::vector<cv::Scalar> CITYSCAPES_COLORS = to_cv_sccalar_colors({{128, 64, 128},
-                                                                               {244, 35, 232},
-                                                                               {70, 70, 70},
-                                                                               {102, 102, 156},
-                                                                               {190, 153, 153},
-                                                                               {153, 153, 153},
-                                                                               {250, 170, 30},
-                                                                               {220, 220, 0},
-                                                                               {107, 142, 35},
-                                                                               {152, 251, 152},
-                                                                               {70, 130, 180},
-                                                                               {220, 20, 60},
-                                                                               {255, 0, 0},
-                                                                               {0, 0, 142},
-                                                                               {0, 0, 70},
-                                                                               {0, 60, 100},
-                                                                               {0, 80, 100},
-                                                                               {0, 0, 230},
-                                                                               {119, 11, 32}});
+static const std::vector<std::vector<uint8_t>> CITYSCAPES_COLORS = {
+    {128, 64, 128}, {244, 35, 232}, {70, 70, 70},    {102, 102, 156}, {190, 153, 153}, {153, 153, 153}, {250, 170, 30},
+    {220, 220, 0},  {107, 142, 35}, {152, 251, 152}, {70, 130, 180},  {220, 20, 60},   {255, 0, 0},     {0, 0, 142},
+    {0, 0, 70},     {0, 60, 100},   {0, 80, 100},    {0, 0, 230},     {119, 11, 32}};
 }  // namespace
 
 int main(int argc, char *argv[]) {
