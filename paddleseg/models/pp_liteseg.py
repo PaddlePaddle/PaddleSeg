ea5eee7fce0d2fd5515a828031069c54404c9592
juncaipeng <13006307475@163.com>
2022-03-09 17:13:58 +0800

    refine pp_liteseg model

--- paddleseg/models/pp_liteseg.py
+++ paddleseg/models/pp_liteseg.py
@@ -33,10 +33,8 @@ class PPLiteSeg(nn.Layer):
             has feat_channels, of which the length is 5.
         backbone_indices (List(int), optional): The values indicate the indices of output of backbone.
             Default: [2, 3, 4].
-        feat_nums (List(int), optinal):  The values indicate the num of tensor used for each fusion module.
-            Default: [1, 1, 1].
-        head_type (str, optional): The head type of PPSeg. Default: PPSegHead.
         arm_type (str, optional): The type of attention refinement module (ARM). Default: ARM_Add_Add.
+        cm_bin_sizes (List(int), optional): The bin size for context module. Default: [1,2,4].
         cm_out_ch (int, optional): The channel of the last context module, which comes after backbone.
             Default: 128.
         arm_out_chs (List(int), optional): The out channels of each arm module. Default: [64, 64, 64].
@@ -47,9 +45,6 @@ class PPLiteSeg(nn.Layer):
                  num_classes,
                  backbone,
                  backbone_indices=[2, 3, 4],
-                 feat_nums=[1, 1, 1],
-                 feat_select_mode='last',
-                 head_type='PPSegHead',
                  arm_type='ARM_Add_SpAttenAdd3',
                  cm_bin_sizes=[1, 2, 4],
                  cm_out_ch=128,
@@ -73,31 +68,18 @@ class PPLiteSeg(nn.Layer):
 
         assert len(backbone_indices) > 1, "The lenght of backbone_indices " \
             "should be greater than 1"
-        assert len(backbone_indices) == len(feat_nums), "The length of " \
-            "backbone_indices and feat_nums should be equal"
-        assert all([x in (1, 2) for x in feat_nums]), "The values in feat_nums " \
-            "should be 1 or 2"
-        assert feat_select_mode in ('last', 'even'), "feat_select_mode " \
-            "should in ('last', 'even')"
         self.backbone_indices = backbone_indices  # [..., x16_id, x32_id]
-        self.feat_nums = feat_nums
-        self.feat_select_mode = feat_select_mode
-        backbone_out_chs = [[backbone.feat_channels[i]] * n \
-            for i, n in zip(backbone_indices, feat_nums)]
+        backbone_out_chs = [backbone.feat_channels[i] for i in backbone_indices]
         print("backbone_out_chs:" + str(backbone_out_chs))
 
         # head
-        head_class = eval(head_type)
-
         if len(arm_out_chs) == 1:
             arm_out_chs = arm_out_chs * len(backbone_indices)
         assert len(arm_out_chs) == len(backbone_indices), "The length of " \
             "arm_out_chs and backbone_indices should be equal"
 
-        self.ppseg_head = head_class(backbone_indices, feat_nums,
-                                     backbone_out_chs, arm_out_chs,
-                                     cm_bin_sizes, cm_out_ch, arm_type,
-                                     resize_mode)
+        self.ppseg_head = PPSegHead(backbone_out_chs, arm_out_chs, cm_bin_sizes,
+                                    cm_out_ch, arm_type, resize_mode)
 
         if len(seg_head_mid_chs) == 1:
             seg_head_mid_chs = seg_head_mid_chs * len(backbone_indices)
@@ -106,8 +88,6 @@ class PPLiteSeg(nn.Layer):
         self.seg_heads = nn.LayerList()  # [..., head_16, head32]
         for in_ch, mid_ch in zip(arm_out_chs, seg_head_mid_chs):
             self.seg_heads.append(SegHead(in_ch, mid_ch, num_classes))
-
-        self.eval_seg_head_id = eval_seg_head_id
 
         # pretrained
         self.pretrained = pretrained
@@ -121,24 +101,7 @@ class PPLiteSeg(nn.Layer):
             f"The nums of backbone feats ({len(feats_backbone)}) should be greater or " \
             f"equal than the nums of backbone_indices ({len(self.backbone_indices)})"
 
-        tmp = []
-        for fs in feats_backbone:
-            fs = fs if isinstance(fs, (list, tuple)) else [fs]
-            tmp.append(fs)
-        feats_backbone = tmp
-
-        feats_selected = []
-        for idx, num in zip(self.backbone_indices, self.feat_nums):
-            fs = feats_backbone[idx]
-            assert len(fs) >= num, "The nums of backbone output feats should be " \
-                "greater than the nums you set"
-            tmp = [fs[-1]]
-            if num == 2:
-                if self.feat_select_mode == 'last':
-                    tmp.insert(0, fs[-2])
-                else:
-                    tmp.insert(0, fs[(len(fs) - 1) // 2])
-            feats_selected.append(tmp)
+        feats_selected = [feats_backbone[i] for i in self.backbone_indices]
 
         feats_head = self.ppseg_head(feats_selected)  # [..., x16, x32]
 
@@ -153,18 +116,8 @@ class PPLiteSeg(nn.Layer):
                 F.interpolate(x, x_hw, mode='bilinear', align_corners=False)
                 for x in logit_list
             ]
-
-            if hasattr(self, 'seg_head_sp2'):
-                logit_list.append(self.seg_head_sp2(feats_backbone[0][-1]))
-            if hasattr(self, 'seg_head_sp4'):
-                logit_list.append(self.seg_head_sp4(feats_backbone[1][-1]))
-            if hasattr(self, 'seg_head_sp8'):
-                logit_list.append(self.seg_head_sp8(feats_backbone[2][-1]))
-            if hasattr(self, 'seg_head_sp16'):
-                logit_list.append(self.seg_head_sp16(feats_backbone[3][-1]))
         else:
-            idx = self.eval_seg_head_id
-            feat_out = self.seg_heads[idx](feats_head[idx])
+            feat_out = self.seg_heads[0](feats_head[0])
             feat_out = F.interpolate(feat_out,
                                      x_hw,
                                      mode='bilinear',
@@ -182,31 +135,15 @@ class PPSegHead(nn.Layer):
     '''
     The head of PPSeg.
     '''
-    def __init__(self, backbone_indices, feat_nums, backbone_out_chs,
-                 arm_out_chs, cm_bin_sizes, cm_out_ch, arm_type, resize_mode):
+    def __init__(self, backbone_out_chs, arm_out_chs, cm_bin_sizes, cm_out_ch,
+                 arm_type, resize_mode):
         super().__init__()
 
-        assert len(backbone_indices) == len(backbone_out_chs), "The length of " \
-            "backbone_indices and backbone_out_chs should be equal"
-        assert len(backbone_indices) == len(arm_out_chs), "The length of " \
-            "backbone_indices and arm_out_chs should be equal"
-
-        self.feat_nums = feat_nums
-        self.resize_mode = resize_mode
-        '''
-        self.cm = nn.Sequential(
-            nn.AdaptiveAvgPool2D(1),
-            layers.ConvBNReLU(
-                backbone_out_chs[-1][-1],
-                cm_out_ch,
-                kernel_size=1,
-                bias_attr=False))
-        '''
-        self.cm = PPContextModule(backbone_out_chs[-1][-1], cm_out_ch,
-                                  cm_out_ch, cm_bin_sizes)
+        self.cm = PPContextModule(backbone_out_chs[-1], cm_out_ch, cm_out_ch,
+                                  cm_bin_sizes)
 
         assert hasattr(layers,arm_type), \
-            "Do not have arm_type ({})".format(arm_type)
+            "Not support arm_type ({})".format(arm_type)
         arm_class = eval("layers." + arm_type)
 
         self.arm_list = nn.LayerList()  # [..., arm8, arm16, arm32]
@@ -231,7 +168,7 @@ class PPSegHead(nn.Layer):
                 The length of in_feat_list and out_feat_list are the same.
         '''
 
-        high_feat = self.cm(in_feat_list[-1][-1])
+        high_feat = self.cm(in_feat_list[-1])
         out_feat_list = []
 
         for i in reversed(range(len(in_feat_list))):
