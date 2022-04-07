b3b1b292e77f36a562a5c23844bc02c33c1f8e19
cc <52520497+juncaipeng@users.noreply.github.com>
2022-04-07 10:50:52 +0800

    [Feature] Add PP-LiteSeg (#1936)

--- predict.py
+++ predict.py
@@ -116,6 +116,8 @@ def parse_args():
 def get_test_config(cfg, args):
 
     test_config = cfg.test_config
+    if 'aug_eval' in test_config:
+        test_config.pop('aug_eval')
     if args.aug_pred:
         test_config['aug_pred'] = args.aug_pred
         test_config['scales'] = args.scales
