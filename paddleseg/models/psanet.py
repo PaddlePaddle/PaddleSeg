36af02576ac8660cfa06c01a8830712d7fe4a8e6
langdu <1207540056@qq.com>
2022-04-25 08:32:50 +0800

    update psanet format

--- paddleseg/models/psanet.py
+++ paddleseg/models/psanet.py
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import paddle
 import paddle.nn as nn
 import paddle.nn.functional as F
 
-import paddle
 from paddleseg.cvlibs import manager
 from paddleseg.models import layers
 from paddleseg.utils import utils
