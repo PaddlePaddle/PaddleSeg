2487367b4b58967d14d780a0a292245cf09243b5
chulutao <chulutao@baidu.com>
2022-05-30 20:32:13 +0800

    [TIPC] C++ infer: PaddleInfer add optional choice for downloading. Automatically select inference pictures

--- test_tipc/test_inference_cpp.sh
+++ test_tipc/test_inference_cpp.sh
@@ -3,7 +3,6 @@ source test_tipc/common_func.sh
 
 
 FILENAME=$1
-INFERIMG=$2
 
 dataline=$(cat ${FILENAME})
 lines=(${dataline})
@@ -23,6 +22,12 @@ use_tensorrt_key=$(func_parser_key_cpp "${lines[7]}")
 use_tensorrt_value=$(func_parser_value_cpp "${lines[7]}")
 use_fp16_key=$(func_parser_key_cpp "${lines[8]}")
 use_fp16_value=$(func_parser_value_cpp "${lines[8]}")
+
+if [ ${model_name} == "pphumanseg_lite" ] || [ ${model_name} == "deeplabv3p_resnet50" ] || [ ${model_name} == "fcn_hrnetw18_small" ]; then
+    INFERIMG="test_tipc/cpp/humanseg_demo.jpg"
+else
+    INFERIMG="test_tipc/cpp/cityscapes_demo.png"
+fi
 
 LOG_PATH="./test_tipc/output/infer_cpp"
 mkdir -p ${LOG_PATH}
