4debf0ba72d2ab6433fb7c9f267df74dda0a4f87
Weilong Wu <veyron_wu@163.com>
2022-05-30 11:56:47 +0000

    [Eager] Support all_reduce under eager mode

--- paddleseg/models/emanet.py
+++ paddleseg/models/emanet.py
@@ -19,6 +19,7 @@ import paddle.nn.functional as F
 from paddleseg.models import layers
 from paddleseg.cvlibs import manager
 from paddleseg.utils import utils
+from paddle.fluid.framework import in_dygraph_mode, _in_legacy_dygraph
 
 
 @manager.MODELS.add_component
@@ -209,7 +210,10 @@ class EMAU(nn.Layer):
             mu = F.normalize(mu, axis=1, p=2)
             mu = self.mu * (1 - self.momentum) + mu * self.momentum
             if paddle.distributed.get_world_size() > 1:
-                mu = paddle.distributed.all_reduce(mu)
+                if in_dygraph_mode():
+                    paddle.distributed.all_reduce(mu)
+                elif _in_legacy_dygraph():
+                    mu = paddle.distributed.all_reduce(mu)
                 mu /= paddle.distributed.get_world_size()
             self.mu = mu
 
