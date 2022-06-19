590a89365f9eaa190d13d33b1013f7a3452bfaef
sun222 <30310694+sun222@users.noreply.github.com>
2022-06-19 16:41:44 +0800

    TIPC 修改

--- contrib/MedicalSeg/test_tipc/prepare.sh
+++ contrib/MedicalSeg/test_tipc/prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ./PaddleSeg/contrib/MedicalSeg/test_tipc/common_func.sh
+source ./test_tipc/common_func.sh
 
 FILENAME=$1
 # MODE be one of ['lite_train_lite_infer']
@@ -19,6 +19,6 @@ trainer_list=$(func_parser_value "${lines[14]}")
 
 # MODE be one of ['lite_train_lite_infer']
 if [ ${MODE} = "lite_train_lite_infer" ];then
-    rm -rf ./PaddleSeg/contrib/MedicalSeg/test_tipc/data/mini_levir_dataset
-    cd ./PaddleSeg/contrib/MedicalSeg/test_tipc/data/ && unzip mini_brainT_dataset.zip && cd ../../
+    rm -rf ./test_tipc/data/mini_levir_dataset
+    cd ./test_tipc/data/ && unzip mini_brainT_dataset.zip && cd ../../
 fi
