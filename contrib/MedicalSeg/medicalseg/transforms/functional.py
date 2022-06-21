b0c0e00388b7524196c889a2fc1573947c43bdc5
sun222 <30310694+sun222@users.noreply.github.com>
2022-06-21 17:06:30 +0800

    [MedicalSeg] Add Unetr (#2238)

--- contrib/MedicalSeg/medicalseg/transforms/functional.py
+++ contrib/MedicalSeg/medicalseg/transforms/functional.py
@@ -129,3 +129,37 @@ def extract_connect_compoent(binary_mask, minimum_volume=0):
             sitk.ConnectedComponent(sitk.GetImageFromArray(binary_mask)),
             minimumObjectSize=minimum_volume))
     return instance_mask
+
+
+def rotate_4d(img, r_plane, angle, order=1, cval=0):
+    """
+    rotate 4D image by r_plane and angle.
+    r_plane (2-list): rotate planes by axis, i.e, [0, 1] or [1, 2] or [0, 2]
+    angle (int): rotate degrees
+    """
+    img = scipy.ndimage.rotate(
+        img,
+        angle=angle,
+        axes=tuple(r_plane),
+        order=order,
+        cval=cval,
+        reshape=False)
+    return img
+
+
+def crop_4d(img, i, j, k, d, h, w):
+    """Crop the given PIL Image.
+    Args:
+        img (numpy ndarray): Image to be cropped.
+        i: Upper pixel coordinate.
+        j: Left pixel coordinate.
+        k:
+        d:
+        h: Height of the cropped image.
+        w: Width of the cropped image.
+    Returns:
+        numpy ndarray: Cropped image.
+    """
+    if not _is_numpy_image(img):
+        raise TypeError('img should be numpy image. Got {}'.format(type(img)))
+    return img[:, i:i + d, j:j + h, k:k + w]
