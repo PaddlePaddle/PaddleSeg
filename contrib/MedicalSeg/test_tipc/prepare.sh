3efe34efd0b02b28452b6b495156a26224cbfe75
sheyan21@126.com <45915150+YellowLight021@users.noreply.github.com>
2022-08-11 21:44:17 +0800

    tipc success

--- contrib/MedicalSeg/test_tipc/prepare.sh
+++ contrib/MedicalSeg/test_tipc/prepare.sh
@@ -13,14 +13,28 @@ IFS=$'\n'
 lines=(${dataline})
 
 # The training params
-model_name=$(func_parser_value "${lines[1]}")
+model_name=$(func_parser_value ${lines[1]})
+
 
 trainer_list=$(func_parser_value "${lines[14]}")
 
 # MODE be one of ['lite_train_lite_infer']
-if [ ${MODE} = "lite_train_lite_infer" ];then
+if [ ${MODE} == "lite_train_lite_infer" ]
+then
+  if [ "${model_name:0:${#model_name}-1}" == "nnFormer" ]
+  then
+    echo "nnFormer"
     mkdir -p ./test_tipc/data
     rm -rf ./test_tipc/data/mini_acdc
     cd ./test_tipc/data/
+    wget https://paddleseg.bj.bcebos.com/dataset/mini_acdc.zip
     unzip mini_acdc.zip  && cd ../../
+  else
+    echo "else"
+    mkdir -p ./test_tipc/data
+    rm -rf ./test_tipc/data/mini_levir_dataset
+    cd ./test_tipc/data/
+    wget https://bj.bcebos.com/paddleseg/dataset/mini_brainT_dataset.zip
+    unzip mini_brainT_dataset.zip  && cd ../../
+  fi
 fi
