1dd6b9495213b276ee06bfd79f06f214e0ab17a6
Bobholamovic <mhlin425@whu.edu.cn>
2022-08-12 16:58:11 +0800

    [Fix] Fix TIPC

--- contrib/MedicalSeg/test_tipc/prepare.sh
+++ contrib/MedicalSeg/test_tipc/prepare.sh
@@ -3,32 +3,33 @@
 source ./test_tipc/common_func.sh
 
 FILENAME=$1
+
 # MODE be one of ['lite_train_lite_infer']
 MODE=$2
 dataline=$(cat ${FILENAME})
+
 # Parser params
 IFS=$'\n'
 lines=(${dataline})
+
 # The training params
 model_name=$(func_parser_value ${lines[1]})
 trainer_list=$(func_parser_value "${lines[14]}")
+
 # MODE be one of ['lite_train_lite_infer']
-if [ ${MODE} = "lite_train_lite_infer" ]
-then
-  if [ "${model_name}" = "nnFormer" ]
-  then
-    echo "nnFormer"
-    mkdir -p ./test_tipc/data
+if [ ${MODE} = "lite_train_lite_infer" ]; then
+  mkdir -p ./test_tipc/data
+
+  if [ "${model_name}" = "nnFormer" ]; then
     rm -rf ./test_tipc/data/mini_acdc
     cd ./test_tipc/data/
     wget https://paddleseg.bj.bcebos.com/dataset/mini_acdc.zip
     unzip mini_acdc.zip  && cd ../../
   else
-    echo "else"
-    mkdir -p ./test_tipc/data
     rm -rf ./test_tipc/data/mini_levir_dataset
     cd ./test_tipc/data/
     wget https://bj.bcebos.com/paddleseg/dataset/mini_brainT_dataset.zip
     unzip mini_brainT_dataset.zip  && cd ../../
   fi
+
 fi
