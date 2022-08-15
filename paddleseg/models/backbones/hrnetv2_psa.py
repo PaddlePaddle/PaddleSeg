f1173ffeaebc79fcd05a9b3f003fd7795e749a02
marshall-dteach <1250859983@qq.com>
2022-08-15 13:12:54 +0800

    config backbone initialize

--- paddleseg/models/backbones/hrnetv2_psa.py
+++ paddleseg/models/backbones/hrnetv2_psa.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import math
+import warnings
 import numpy as np
 
 import paddle
@@ -72,13 +74,20 @@ class PolarizedSelfAttentionModule(nn.Layer):
             padding=0,
             bias_attr=False)
         self.softmax_left = nn.Softmax(axis=2)
-        #self.reset_parameters()
-        self.init_weight()
 
-    def init_weight(self):
-        for layer in self.sublayers():
-            if isinstance(layer, nn.Conv2D):
-                param_init.kaiming_normal_init(layer.weight)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.kaiming_init(self.conv_q_right, mode='fan_in')
+        self.kaiming_init(self.conv_v_right, mode='fan_in')
+        self.kaiming_init(self.conv_q_left, mode='fan_in')
+        self.kaiming_init(self.conv_v_left, mode='fan_in')
+        self.kaiming_init(self.conv_up, mode='fan_in')
+        self.conv_q_right.inited = True
+        self.conv_v_right.inited = True
+        self.conv_q_left.inited = True
+        self.conv_v_left.inited = True
+        self.conv_up.inited = True
 
     def spatial_pool(self, x):
         input_x = self.conv_v_right(x)
@@ -115,6 +124,117 @@ class PolarizedSelfAttentionModule(nn.Layer):
         context_spatial = self.channel_pool(x)
         out = context_spatial + context_channel
         return out
+
+    def calculate_gain(self, nonlinearity, param=None):
+        linear_fns = [
+            'linear', 'conv1d', 'conv2d', 'conv3d', 'conv_transpose1d',
+            'conv_transpose2d', 'conv_transpose3d'
+        ]
+        if nonlinearity in linear_fns or nonlinearity == 'sigmoid':
+            return 1
+        elif nonlinearity == 'tanh':
+            return 5.0 / 3
+        elif nonlinearity == 'relu':
+            return math.sqrt(2.0)
+        elif nonlinearity == 'leaky_relu':
+            if param is None:
+                negative_slope = 0.01
+            elif not isinstance(param, bool) and isinstance(
+                    param, int) or isinstance(param, float):
+                # True/False are instances of int, hence check above
+                negative_slope = param
+            else:
+                raise ValueError("negative_slope {} not a valid number".format(
+                    param))
+            return math.sqrt(2.0 / (1 + negative_slope**2))
+        elif nonlinearity == 'selu':
+            return 3.0 / 4  # Value found empirically (https://github.com/pytorch/pytorch/pull/50664)
+        else:
+            raise ValueError("Unsupported nonlinearity {}".format(nonlinearity))
+
+    def _calculate_fan_in_and_fan_out(self, tensor):
+        dimensions = tensor.dim()
+        if dimensions < 2:
+            raise ValueError(
+                "Fan in and fan out can not be computed for tensor with fewer than 2 dimensions"
+            )
+
+        num_input_fmaps = tensor.shape[1]
+        num_output_fmaps = tensor.shape[0]
+        receptive_field_size = 1
+        if tensor.dim() > 2:
+            # math.prod is not always available, accumulate the product manually
+            # we could use functools.reduce but that is not supported by TorchScript
+            for s in tensor.shape[2:]:
+                receptive_field_size *= s
+        fan_in = num_input_fmaps * receptive_field_size
+        fan_out = num_output_fmaps * receptive_field_size
+
+        return fan_in, fan_out
+
+    def _calculate_correct_fan(self, tensor, mode):
+        mode = mode.lower()
+        valid_modes = ['fan_in', 'fan_out']
+        if mode not in valid_modes:
+            raise ValueError("Mode {} not supported, please use one of {}".
+                             format(mode, valid_modes))
+
+        fan_in, fan_out = self._calculate_fan_in_and_fan_out(tensor)
+        return fan_in if mode == 'fan_in' else fan_out
+
+    def kaiming_normal_(self,
+                        tensor,
+                        a=0,
+                        mode='fan_in',
+                        nonlinearity='leaky_relu'):
+        if 0 in tensor.shape:
+            warnings.warn("Initializing zero-element tensors is a no-op")
+            return tensor
+        fan = self._calculate_correct_fan(tensor, mode)
+        gain = self.calculate_gain(nonlinearity, a)
+        std = gain / math.sqrt(fan)
+        with paddle.no_grad():
+            initializer = paddle.nn.initializer.Normal(mean=0, std=std)
+            initializer(tensor)
+
+    def kaiming_uniform_(self,
+                         tensor,
+                         a=0,
+                         mode='fan_in',
+                         nonlinearity='leaky_relu'):
+        if 0 in tensor.shape:
+            warnings.warn("Initializing zero-element tensors is a no-op")
+            return tensor
+        fan = self._calculate_correct_fan(tensor, mode)
+        gain = self.calculate_gain(nonlinearity, a)
+        std = gain / math.sqrt(fan)
+        bound = math.sqrt(
+            3.0) * std  # Calculate uniform bounds from standard deviation
+        with paddle.no_grad():
+            initializer = paddle.nn.initializer.Uniform(-bound, bound)
+        initializer(tensor)
+
+    def constant_(self, tensor, a):
+        with paddle.no_grad():
+            initializer = paddle.nn.initializer.Constant(value=a)
+            initializer(tensor)
+
+    def kaiming_init(self,
+                     module,
+                     a=0,
+                     mode='fan_out',
+                     nonlinearity='relu',
+                     bias=0,
+                     distribution='normal'):
+        assert distribution in ['uniform', 'normal']
+        if distribution == 'uniform':
+            self.kaiming_uniform_(
+                module.weight, a=a, mode=mode, nonlinearity=nonlinearity)
+        else:
+            self.kaiming_normal_(
+                module.weight, a=a, mode=mode, nonlinearity=nonlinearity)
+        if hasattr(module, 'bias') and module.bias is not None:
+            self.constant_(module.bias, bias)
 
 
 class HRNetBasicBlock(nn.Layer):
