96a278a10e5127188b1b52eb584f671006138604
shiyutang <34859558+shiyutang@users.noreply.github.com>
2022-08-23 16:03:14 +0800

    Update l1_loss.py

--- paddleseg/models/losses/l1_loss.py
+++ paddleseg/models/losses/l1_loss.py
@@ -41,6 +41,7 @@ class L1Loss(nn.L1Loss):
             If `reduction` is ``'mean'``, the reduced mean loss is returned.
             If `reduction` is ``'sum'``, the reduced sum loss is returned.
             Default is ``'mean'``.
+        ignore_index (int, optional): Specifies a target value that is ignored and does not contribute to the input gradient. Default: 255.
     Shape:
         input (Tensor): The input tensor. The shapes is [N, *], where N is batch size and `*` means any number of additional dimensions. It's data type should be float32, float64, int32, int64.
         label (Tensor): label. The shapes is [N, *], same shape as ``input`` . It's data type should be float32, float64, int32, int64.
