878d5e3634cf6361cda4122a806afb0fe3452ff5
marshall-dteach <1250859983@qq.com>
2022-08-31 15:57:26 +0800

    in_channels2

--- paddleseg/models/backbones/hrnetv2_psa.py
+++ paddleseg/models/backbones/hrnetv2_psa.py
@@ -699,7 +699,6 @@ class HighResolutionNet(nn.Layer):
 @manager.BACKBONES.add_component
 def HRNetV2_PSA(**kwargs):
     model = HighResolutionNet(
-        in_channels=3,
         stage1_num_channels=[64],
         stage1_num_blocks=[4],
         stage2_num_channels=[48, 96],
