40f7891d2542d5f6a42881fd38f7f3290d05a1df
sheyan21@126.com <45915150+YellowLight021@users.noreply.github.com>
2022-09-19 16:39:47 +0800

    hehe

--- contrib/MedicalSeg/medicalseg/transforms/functional.py
+++ contrib/MedicalSeg/medicalseg/transforms/functional.py
@@ -185,6 +185,7 @@ def augment_gaussian_noise(data_sample: np.ndarray,
                 0.0, variance_here, size=data_sample[c].shape)
     return data_sample
 
+
 def get_range_val(value, rnd_type="uniform"):
     if isinstance(value, (list, tuple, np.ndarray)):
         if len(value) == 2:
@@ -334,7 +335,8 @@ def augment_linear_downsampling_scipy(data_sample,
             if per_channel:
                 if isinstance(zoom_range[0], (tuple, list, np.ndarray)):
                     assert len(zoom_range) == dim
-                    zoom = np.array([np.random.uniform(i[0], i[1]) for i in zoom_range])
+                    zoom = np.array(
+                        [np.random.uniform(i[0], i[1]) for i in zoom_range])
                 else:
                     zoom = np.random.uniform(zoom_range[0], zoom_range[1])
 
--- contrib/MedicalSeg/medicalseg/utils/utils.py
+++ contrib/MedicalSeg/medicalseg/utils/utils.py
@@ -260,5 +260,3 @@ def wrapped_partial(func, *args, **kwargs):
     partial_func = partial(func, *args, **kwargs)
     update_wrapper(partial_func, func)
     return partial_func
-
-
