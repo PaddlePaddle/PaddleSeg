b9175492232350b596f359fad9c01fd71ad3677b
shiyutang <34859558+shiyutang@users.noreply.github.com>
2022-09-20 14:48:28 +0800

    [Feature] Add EISeg-Med3D (#2526)
    * EISeg-Med3D

---
+++ EISeg/med3d/EISegMed3D/inference/models.py
@@ -0,0 +1,486 @@
+import os
+import sys
+
+sys.path.append(
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), "./.."))
+
+import paddle
+import paddle.nn as nn
+import numpy as np
+
+from inference.ops import DistMaps3D, ScaleLayer, BatchImageNormalize3D
+
+
+class ISModel3D(nn.Layer):
+    def __init__(
+            self,
+            use_rgb_conv=True,
+            with_aux_output=False,
+            norm_radius=2,
+            use_disks=False,
+            cpu_dist_maps=False,
+            clicks_groups=None,
+            with_prev_mask=False,  # True
+            use_leaky_relu=False,
+            binary_prev_mask=False,
+            conv_extend=False,
+            norm_layer=nn.BatchNorm3D,
+            norm_mean_std=(
+                [0.00040428873, ],
+                [0.00059983705, ],
+            ), ):  #  image.std(): [0.00053328] image.mean() [0.00023692])
+        super().__init__()
+
+        self.with_aux_output = with_aux_output
+        self.clicks_groups = clicks_groups
+        self.with_prev_mask = with_prev_mask
+        self.binary_prev_mask = binary_prev_mask
+        self.normalization = BatchImageNormalize3D(norm_mean_std[0],
+                                                   norm_mean_std[1])
+
+        self.coord_feature_ch = 2
+        if clicks_groups is not None:
+            self.coord_feature_ch *= len(clicks_groups)
+
+        if self.with_prev_mask:
+            self.coord_feature_ch += 1  # 3
+
+        if use_rgb_conv:
+            rgb_conv_layers = [
+                nn.Conv3D(
+                    in_channels=1 + self.coord_feature_ch,
+                    out_channels=6 + self.coord_feature_ch,
+                    kernel_size=1),
+                norm_layer(6 + self.coord_feature_ch),
+                nn.LeakyReLU(negative_slope=0.2)
+                if use_leaky_relu else nn.ReLU(),
+                nn.Conv3D(
+                    in_channels=6 + self.coord_feature_ch,
+                    out_channels=1,
+                    kernel_size=1),
+            ]
+            self.rgb_conv = nn.Sequential(*rgb_conv_layers)
+
+        elif conv_extend:
+            self.rgb_conv = None
+            self.maps_transform = nn.Conv3D(
+                in_channels=self.coord_feature_ch,
+                out_channels=64,
+                kernel_size=3,
+                stride=2,
+                padding=1)
+        else:
+            self.rgb_conv = None
+            mt_layers = [
+                nn.Conv3D(
+                    in_channels=self.coord_feature_ch,
+                    out_channels=16,
+                    kernel_size=1),
+                nn.LeakyReLU(negative_slope=0.2)
+                if use_leaky_relu else nn.ReLU(),
+                nn.Conv3D(
+                    in_channels=16,
+                    out_channels=16,
+                    kernel_size=3,
+                    stride=1,
+                    padding=1),
+                ScaleLayer(
+                    init_value=0.05, lr_mult=1),
+            ]
+            self.maps_transform = nn.Sequential(*mt_layers)
+        if self.clicks_groups is not None:
+            self.dist_maps = nn.LayerList()
+            for click_radius in self.clicks_groups:
+                self.dist_maps.append(
+                    DistMaps3D(
+                        norm_radius=click_radius,
+                        spatial_scale=1.0,
+                        cpu_mode=cpu_dist_maps,
+                        use_disks=use_disks))
+        else:
+            self.dist_maps = DistMaps3D(
+                norm_radius=norm_radius,
+                spatial_scale=1.0,
+                cpu_mode=cpu_dist_maps,
+                use_disks=use_disks)
+
+    def forward(self, image, coord_features):
+        if self.rgb_conv is not None:
+            x = self.rgb_conv(paddle.concat(
+                (image, coord_features), axis=1))  # [B, 4, H, W, D] #
+            outputs = self.backbone_forward(x)
+        else:
+            coord_features = self.maps_transform(
+                coord_features)  # [B, 3, H, W, D]
+            outputs = self.backbone_forward(image, coord_features)
+
+        outputs["instances"] = nn.functional.interpolate(
+            outputs["instances"],
+            size=paddle.shape(image)[2:],  # [4, 20, 512, 512, 12]
+            mode="trilinear",
+            align_corners=True,
+            data_format="NCDHW", )  # image [4  , 1  , 512, 512, 12 ]
+        if self.with_aux_output:
+            outputs["instances_aux"] = nn.functional.interpolate(
+                outputs["instances_aux"],
+                size=paddle.shape(image)[2:],
+                mode="biltrilinearinear",
+                align_corners=True,
+                data_format="NCDHW", )
+        return outputs
+
+    def prepare_input(self, image):
+        prev_mask = None
+        if self.with_prev_mask:
+            prev_mask = paddle.slice(
+                image,
+                axes=[1, ],
+                starts=[1, ],
+                ends=[1000, ], )
+            image = paddle.slice(
+                image,
+                axes=[1, ],
+                starts=[0, ],
+                ends=[1, ], )
+            # prev_mask = image[:, 1:, :, :, :]
+            # image = image[:, :1, :, :, :]
+            if self.binary_prev_mask:
+                prev_mask = (prev_mask > 0.5).astype("float32")
+
+        image = self.normalization(image)  # why?
+        return image, prev_mask
+
+    def backbone_forward(self, image, coord_features=None):
+        raise NotImplementedError
+
+    def get_coord_features(self, image, prev_mask, points):
+
+        coord_features = self.dist_maps(
+            image,
+            points)  #  [16, 1, 512, 512, 12], [16, 48, 4]. # [B, 2, H, W, D]
+
+        if prev_mask is not None:
+            coord_features = paddle.concat(
+                (prev_mask, coord_features), axis=1)  # [B, 3, H, W, D]
+
+        return coord_features
+
+
+def split_points_by_order(tpoints,
+                          groups):  # todo check if point have dimension problem
+    points = tpoints.numpy()
+    num_groups = len(groups)
+    bs = points.shape[0]
+    num_points = points.shape[1] // 2
+
+    groups = [x if x > 0 else num_points for x in groups]
+    group_points = [
+        np.full(
+            (bs, 2 * x, 3), -1, dtype=np.float32) for x in groups
+    ]
+
+    last_point_indx_group = np.zeros((bs, num_groups, 2), dtype=np.int)
+    for group_indx, group_size in enumerate(groups):
+        last_point_indx_group[:, group_indx, 1] = group_size
+
+    for bindx in range(bs):
+        for pindx in range(2 * num_points):
+            point = points[bindx, pindx, :]
+            group_id = int(point[2])
+            if group_id < 0:
+                continue
+
+            is_negative = int(pindx >= num_points)
+            if group_id >= num_groups or (
+                    group_id == 0 and
+                    is_negative):  # disable negative first click
+                group_id = num_groups - 1
+
+            new_point_indx = last_point_indx_group[bindx, group_id, is_negative]
+            last_point_indx_group[bindx, group_id, is_negative] += 1
+
+            group_points[group_id][bindx, new_point_indx, :] = point
+
+    group_points = [
+        paddle.to_tensor(
+            x, dtype=tpoints.dtype) for x in group_points
+    ]
+
+    return group_points
+
+
+from paddleseg.utils import utils
+
+
+class LUConv(nn.Layer):
+    def __init__(self, nchan, elu):
+        super(LUConv, self).__init__()
+        self.relu1 = nn.ELU() if elu else nn.PReLU(nchan)
+        self.conv1 = nn.Conv3D(nchan, nchan, kernel_size=5, padding=2)
+
+        self.bn1 = nn.BatchNorm3D(nchan)
+
+    def forward(self, x):
+        out = self.relu1(self.bn1(self.conv1(x)))
+
+        return out
+
+
+def _make_nConv(nchan, depth, elu):
+    """
+    Make depth number of layer(convbnrelu) and don't change the channel
+    Add Nonlinearity into the network
+    """
+    layers = []
+    for _ in range(depth):
+        layers.append(LUConv(nchan, elu))
+    return nn.Sequential(*layers)
+
+
+class InputTransition(nn.Layer):
+    """
+    Transfer the input into 16 channels + tiled input
+    """
+
+    def __init__(self, in_channels, elu):
+        super(InputTransition, self).__init__()
+        self.num_features = 16
+        self.in_channels = in_channels
+
+        self.conv1 = nn.Conv3D(
+            self.in_channels, self.num_features, kernel_size=5, padding=2)
+
+        self.bn1 = nn.BatchNorm3D(self.num_features)
+
+        self.relu1 = nn.ELU() if elu else nn.PReLU(self.num_features)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        repeat_rate = int(self.num_features / self.in_channels)
+        out = self.bn1(out)
+        x_tile = x.tile([1, repeat_rate, 1, 1, 1])
+        return self.relu1(paddle.add(out, x_tile))
+
+
+class DownTransition(nn.Layer):
+    def __init__(self,
+                 inChans,
+                 nConvs,
+                 elu,
+                 dropout=False,
+                 downsample_stride=(2, 2, 2),
+                 kernel=(2, 2, 2)):
+        """
+        1. double the output channel and downsample the input using down_conv(the kernel size can be changed)
+        2. add dropout by option
+        3. add nConvs layer to add linearity and add with original downsample one
+        """
+        super(DownTransition, self).__init__()
+        outChans = 2 * inChans
+        self.if_dropout = dropout
+        self.down_conv = nn.Conv3D(
+            inChans, outChans, kernel_size=kernel, stride=downsample_stride)
+        self.bn1 = nn.BatchNorm3D(outChans)
+        self.relu1 = nn.ELU() if elu else nn.PReLU(outChans)
+        self.relu2 = nn.ELU() if elu else nn.PReLU(outChans)
+        self.dropout = nn.Dropout3D()
+        self.ops = _make_nConv(outChans, nConvs, elu)
+
+    def forward(self, x):
+        down = self.relu1(self.bn1(self.down_conv(x)))
+        out = self.dropout(down) if self.if_dropout else down
+        out = self.ops(out)
+        out = paddle.add(out, down)
+        out = self.relu2(out)
+
+        return out
+
+
+class UpTransition(nn.Layer):
+    def __init__(
+            self,
+            inChans,
+            outChans,
+            nConvs,
+            elu,
+            dropout=False,
+            dropout2=False,
+            upsample_stride_size=(2, 2, 2),
+            kernel=(2, 2, 2), ):
+        super(UpTransition, self).__init__()
+        """
+        1. Add dropout to input and skip input optionally (generalization)
+        2. Use Conv3DTranspose to upsample (upsample)
+        3. concate the upsampled and skipx (multi-leval feature fusion)
+        4. Add nConvs convs and residually add with result of step(residual + nonlinearity)
+        """
+        self.up_conv = nn.Conv3DTranspose(
+            inChans,
+            outChans // 2,
+            kernel_size=kernel,
+            stride=upsample_stride_size)
+
+        self.bn1 = nn.BatchNorm3D(outChans // 2)
+        self.relu1 = nn.ELU() if elu else nn.PReLU(outChans // 2)
+        self.relu2 = nn.ELU() if elu else nn.PReLU(outChans)
+        self.if_dropout = dropout
+        self.if_dropout2 = dropout2
+        self.dropout1 = nn.Dropout3D()
+        self.dropout2 = nn.Dropout3D()
+        self.ops = _make_nConv(outChans, nConvs, elu)
+
+    def forward(self, x, skipx):
+        out = self.dropout1(x) if self.if_dropout else x
+        skipx = self.dropout2(skipx) if self.if_dropout2 else skipx
+        out = self.relu1(self.bn1(self.up_conv(out)))
+        xcat = paddle.concat((out, skipx), 1)
+        out = self.ops(xcat)
+        out = self.relu2(paddle.add(out, xcat))
+
+        return out
+
+
+class OutputTransition(nn.Layer):
+    def __init__(self, in_channels, num_classes, elu):
+        """
+        conv the output down to channels as the desired classesv
+        """
+        super(OutputTransition, self).__init__()
+        self.conv1 = nn.Conv3D(
+            in_channels, num_classes, kernel_size=5, padding=2)
+        self.bn1 = nn.BatchNorm3D(num_classes)
+        self.relu1 = nn.ELU() if elu else nn.PReLU(num_classes)
+
+        self.conv2 = nn.Conv3D(num_classes, num_classes, kernel_size=1)
+
+    def forward(self, x):
+        out = self.relu1(self.bn1(self.conv1(x)))
+        out = self.conv2(out)
+        return out
+
+
+class VNet(nn.Layer):
+    """
+    Implementations based on the Vnet paper: https://arxiv.org/abs/1606.04797
+    """
+
+    def __init__(
+            self,
+            elu=False,
+            in_channels=1,
+            num_classes=2,
+            pretrained=None,
+            kernel_size=((2, 2, 2), (2, 2, 2), (2, 2, 2), (2, 2, 2)),
+            stride_size=((2, 2, 2), (2, 2, 2), (2, 2, 2), (2, 2, 2)), ):
+        super().__init__()
+        self.best_loss = 1000000
+        self.num_classes = num_classes
+        self.in_channels = in_channels
+
+        self.in_tr = InputTransition(in_channels, elu=elu)
+        self.down_tr32 = DownTransition(
+            16, 1, elu, downsample_stride=stride_size[0], kernel=kernel_size[0])
+        self.down_tr64 = DownTransition(
+            32, 2, elu, downsample_stride=stride_size[1], kernel=kernel_size[1])
+        self.down_tr128 = DownTransition(
+            64,
+            3,
+            elu,
+            dropout=True,
+            downsample_stride=stride_size[2],
+            kernel=kernel_size[2])
+        self.down_tr256 = DownTransition(
+            128,
+            2,
+            elu,
+            dropout=True,
+            downsample_stride=stride_size[3],
+            kernel=kernel_size[3])
+        self.up_tr256 = UpTransition(
+            256,
+            256,
+            2,
+            elu,
+            dropout=True,
+            dropout2=True,
+            upsample_stride_size=stride_size[3],
+            kernel=kernel_size[3])
+        self.up_tr128 = UpTransition(
+            256,
+            128,
+            2,
+            elu,
+            dropout=True,
+            dropout2=True,
+            upsample_stride_size=stride_size[2],
+            kernel=kernel_size[2])
+        self.up_tr64 = UpTransition(
+            128,
+            64,
+            1,
+            elu,
+            upsample_stride_size=stride_size[1],
+            kernel=kernel_size[1])
+        self.up_tr32 = UpTransition(
+            64,
+            32,
+            1,
+            elu,
+            upsample_stride_size=stride_size[0],
+            kernel=kernel_size[0])
+        self.out_tr = OutputTransition(32, num_classes, elu)
+
+        self.pretrained = pretrained
+        self.init_weight()
+
+    def init_weight(self):
+        if self.pretrained is not None:
+            utils.load_entire_model(self, self.pretrained)
+
+    def forward(self, x, additional_features):  # [4, 1, 512, 512, 12]
+        x = self.in_tr(x)  # dropout cause a lot align problem
+
+        if additional_features is not None:  # todo check shape [B, 16, H, W, D] # [4, 16, 512, 512, 12] #
+            x = x + additional_features
+
+        out32 = self.down_tr32(x)  # [4, 32, 256, 256, 9]
+        out64 = self.down_tr64(out32)  # [4, 64, 128, 128, 8]
+        out128 = self.down_tr128(out64)  # [4, 128, 64, 64, 4]
+        out256 = self.down_tr256(out128)  # [4, 256, 32, 32, 2]
+        out = self.up_tr256(out256, out128)  # [4, 256, 64, 64, 4]
+        out = self.up_tr128(out, out64)  # [4, 128, 128, 128, 8]
+        out = self.up_tr64(out, out32)  # [4, 64, 256, 256, 9]
+        out = self.up_tr32(out, x)  # [4, 32, 512, 512, 12]
+        out = self.out_tr(out)  # [4, num_classes, 512, 512, 12]
+        return out
+
+
+class VNetModel(ISModel3D):
+    # @serialize
+    def __init__(self,
+                 elu=False,
+                 in_channels=1,
+                 num_classes=2,
+                 pretrained=None,
+                 kernel_size=((2, 2, 2), (2, 2, 2), (2, 2, 2), (2, 2, 2)),
+                 stride_size=((2, 2, 2), (2, 2, 2), (2, 2, 2), (2, 2, 2)),
+                 norm_layer=nn.BatchNorm3D,
+                 **kwargs):
+        super().__init__(norm_layer=norm_layer, **kwargs)
+
+        self.feature_extractor = VNet(
+            elu=elu,
+            in_channels=in_channels,
+            num_classes=num_classes,
+            pretrained=pretrained,
+            kernel_size=kernel_size,
+            stride_size=stride_size, )  # diff: 去除了backbone mult，因为没有backbone
+
+    def backbone_forward(self, image, coord_features=None):
+        backbone_features = self.feature_extractor(
+            image, coord_features)  # todo ：增加对点特征的融合
+
+        return {
+            "instances": backbone_features,
+            "instances_aux": backbone_features,
+        }  # result: 直接输出最后多少类别的分类tensor  # [4, num_classes , 512, 512, 12]
---
+++ EISeg/med3d/README.md
@@ -0,0 +1,202 @@
+简体中文 | [English](README_en.md)
+
+<div align="center">
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/34859558/188449455-cd4e4099-6e70-44ca-b8de-57bab04c187c.png" align="middle" width = 500" />
+</p>
+
+**专注用户友好、高效、智能的3D医疗图像标注平台**  <img src="https://user-images.githubusercontent.com/34859558/188409382-467c4c45-df5f-4390-ac40-fa24149d4e16.png" width="30"/>
+
+[![License](https://img.shields.io/badge/license-Apache%202-blue.svg)](LICENSE)
+![python version](https://img.shields.io/badge/python-3.6+-orange.svg)
+![support os](https://img.shields.io/badge/os-linux%2C%20win%2C%20mac-yellow.svg)
+
+</div>
+
+##  <img src="https://user-images.githubusercontent.com/34859558/188422593-4bc47c72-866a-4374-b9ed-1308c3453165.png" width="30"/> 简介
+3D 医疗数据标注是训练 3D 图像分割模型的重要一环，但 3D 医疗数据标注依赖专业人士进行费时费力的手工标注。 标注效率的低下导致了大规模标注数据的缺乏，从而严重阻碍了医疗AI的发展。为了解决这个问题，我们推出了基于交互式分割的3D医疗图像智能标注平台 EISeg-Med3D。
+
+EISeg-Med3D 是一个用于智能医学图像分割的 3D Slicer 插件，通过使用训练的交互式分割 AI 模型来进行交互式医学图像标注。它安装简单、使用方便，结合高精度的预测模型，可以获取比手工标注**数十倍**的效率提升。目前我们的医疗标注提供了在指定的 [MRI 椎骨数据](https://aistudio.baidu.com/aistudio/datasetdetail/81211)上的使用体验，如果有其他数据上的3D标注需求，可以[联系我们](https://github.com/PaddlePaddle/PaddleSeg/issues/new/choose)。
+
+
+<div align="center">
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/34859558/188415269-10526530-0415-4632-8223-0e5d755db29c.gif"  align="middle" width = 900"/>
+</p>
+</div>
+
+
+## <img src="https://user-images.githubusercontent.com/34859558/188419267-bd117697-7456-4c72-8cbe-1272264d4fe4.png" width="30"/> 特性
+* **高效**：每个类别只需**数次点击**直接生成3d分割结果，从此告别费时费力的手工标注。
+
+* **准确**：点击 3 点单物体 mIOU 即可达到 **0.85**，配合搭载机器学习算法和手动标注的标注编辑器，精度 100% 不是梦。
+
+* **便捷**：三步轻松安装；标注结果、进度自动保存；标注结果透明度调整提升标注准确度；用户友好的界面交互，让你标注省心不麻烦。
+
+*************
+
+## 目录结构
+0. [最新消息](##最新消息)
+1. [EISeg-Med3D 模型介绍](##EISeg-Med3D模型介绍)
+2. [使用指南](##使用指南)
+3. [TODO](##TODO)
+4. [License](##License)
+5. [致谢](##致谢)
+
+
+## <img src="https://user-images.githubusercontent.com/34859558/190043516-eed25535-10e8-4853-8601-6bcf7ff58197.png" width="30"/> 最新消息
+- [2022-09] EISeg-Med3D 正式发布，包含在指定椎骨数据上的高精度模型的**用户友好、高效、智能的3D医疗图像标注平台**。
+
+## <img src="https://user-images.githubusercontent.com/34859558/190049708-7a1cee3c-322b-4263-9ed0-23051825b1a6.png" width="30"/> EISeg-Med3D 模型介绍
+EISeg-Med3D模型结构如下图所示，我们创新性地将3D模型引入医疗交互式分割中，并修改 RITM 的采点模块和生成点击特征模块和3D数据兼容，从而直接进行3D医学图像的标注，从模型层面在2D标注的基础上实现标注的**更精准，更高效**。
+
+整体模型包含点击生成模块、点击特征生成模块、点击特征和输入图像融合和分割模个部分：
+* 点击生成模块 3D click sampler：直接基于 3D 标签数据进行正负点采样，其中正点为3D目标体中心位置随机点，负点为3D目标体边缘随机采点。
+* 点击特征生成模块 3D feature extractor：在生成点击之后，为了扩大点击的影响范围，通过disk的形式在原有点击的基础上生成半径R的圆球，扩大特征覆盖范围。
+* 点击特征和输入图像融合：将输入图像和生产的点击特征经过卷积块进行重新映射和进行相加融合，从而网络同时获取图像和点击信息，并对特定区域进行3D分割获得标注结果。
+* 分割模型：分割模型沿用 3D 分割模型Vnet，在图像和点击信息综合下生成如图所示的预测结果，并在Dice损失和CE损失的约束下逼近真实结果。从而在预测阶段，输入图像和指定点击后基于点击目标生成期望的标注结果。
+
+<p align="center">
+<img src="https://user-images.githubusercontent.com/34859558/190861789-793bd9f3-17a8-49d6-a2a7-bce82696d28e.png" width="80.6%" height="20%">
+<p align="center">
+ EISeg-Med3D模型
+</p>
+</p>
+
+##  <img src="https://user-images.githubusercontent.com/34859558/188439970-18e51958-61bf-4b43-a73c-a9de3eb4fc79.png" width="30"/> 使用指南
+EISeg-Med3D 的使用整体流程如下图所示，我们将按照环境安装、模型下载和使用步骤三部分说明，其中使用步骤也可以参见简介中的视频。
+
+
+<p align="center">
+<img src="https://user-images.githubusercontent.com/48357642/187884472-32e6dd36-be7b-4b32-b5d1-c0ccd743e1ca.png" width="60.6%" height="20%">
+<p align="center">
+ 整体使用流程
+</p>
+</p>
+
+
+<p align="center">
+<img src="https://user-images.githubusercontent.com/48357642/187884776-470195d6-46d1-4e2b-8403-0cb320f185ec.png" width="80.6%" height="60%">
+<p align="center">
+智能标注模块流程
+</p>
+</p>
+
+
+### 1. 环境安装
+
+1. 下载并安装3D slicer软件：[3D slicer 官网](https://www.slicer.org/)
+
+2. 下载 EISeg-Med3D 代码：
+```
+git clone https://github.com/PaddlePaddle/PaddleSeg.git
+```
+
+3. 安装Paddle，在slicer的python interpreter中，参考[快速安装文档](https://www.paddlepaddle.org.cn/install/quick)安装PaddlePaddle。 比如Windows、CUDA 11.1，安装GPU版本，执行如下命令：
+```
+import sys
+import os
+sys.executable # 'D:/slicer/Slicer 5.0.3/bin/PythonSlicer.exe'
+
+os.system(f"'{sys.executable}' -m pip install paddlepaddle-gpu==2.3.1.post111 -f  https://www.paddlepaddle.org.cn/whl/windows/mkl/avx/stable.html")
+
+```
+<details>
+<summary><b> 常见问题 </b></summary>
+1. 安装PaddlePaddle之后出现下面报错：
+<p align="center">
+<img src="https://user-images.githubusercontent.com/34859558/189288387-4773c35a-ac8e-421d-bfed-2264ac57cda5.png" width="70.6%" height="20%">
+</p>
+
+进入到报错位置所在的 subprocess.py， 修改Popen类的属性 shell=True 即可。
+
+
+</details>
+
+<!-- <details>
+<summary><b>使用步骤</b></summary> -->
+### 2. 模型、数据下载
+目前我们提供在下列模型和数据上的试用体验，可以下载表格中模型和数据到指定目录，并将模型和数据进行解压缩操作用于后续加载：
+<p align="center">
+
+| 数据 | 模型 | 下载链接 |
+|:-:|:-:|:-:|
+| MRI椎骨数据 | 交互式 Vnet |[模型](链接: https://pan.baidu.com/s/1vu0ZIbGumlFvRlMGbMvWAg)-pw: dt8q \|  [椎骨数据](https://aistudio.baidu.com/aistudio/datasetdetail/81211)|
+
+</p>
+
+### 3. 使用步骤
+#### 0. 双击打开 3D Slicer
+
+<p align="center">
+<img src="https://user-images.githubusercontent.com/34859558/190042214-391fbdc4-a007-42d2-9019-f1ff3d97b6eb.png" width="20.6%" height="20%">
+</p>
+
+#### 1. 加载插件
+* 找到 Extension wizard 插件：
+<p align="center">
+<img src="https://user-images.githubusercontent.com/34859558/188458289-b59dc5e3-34eb-4d40-b18b-ce0b35c066c6.png" width="60.6%" height="20%">
+</p>
+
+* 点击 Select Extension，并选择到 EISeg/med3d 目录，并点击加载对应模块，等待 Slicer 进行加载。
+<p align="center">
+<img src="https://user-images.githubusercontent.com/34859558/188458463-066ff0b6-ff80-4d0d-aca0-3b3b12f710ef.png" width="60.6%" height="20%">
+</p>
+
+* 加载完后，切换到 EISegMed3D模块。
+<p align="center">
+<img src="https://user-images.githubusercontent.com/34859558/188458684-46465fed-fdde-43dd-a97c-5da7678f3f99.png" width="60.6%" height="20%">
+</p>
+
+
+#### 2. 加载模型
+* 在```Model Settings```中加载保存在本地的模型，点击```Model Path```路径选择框后面的```...```的按钮，选择后缀名为```.pdodel```的本地文件，点击```Param Path```路径选择框后面的```...```的按钮，选择后缀名为```.pdiparams```的本地文件。
+* 点击```Load Static Model```按钮，此时会有弹窗提示```Sucessfully loaded model to gpu!```，表示模型已经加载进来。
+<p align="center">
+<img src="https://user-images.githubusercontent.com/48357642/187881886-e4d99fb4-c697-48a5-8cd7-a5ab83c7791d.PNG" width="70.6%" height="20%">
+</p>
+
+#### 3. 加载图像
+* 点击```Data Folder```后面的按钮，选择待标注的医学图像文件所在路径后，会自动把该路径下的所有图像全部加载，此时可以在```Progress```中查看加载进来的所有图像以及当前已标注状态。
+
+<p align="center">
+<img src="https://user-images.githubusercontent.com/48357642/187882370-6f9a8f21-8a96-4a4c-8451-18d6e608f7e4.PNG" width="70.6%" height="20%">
+</p>
+
+#### 4. 开始标注
+* 在```Segment Editor```中点击```Add/Remove```按钮便可自行添加标签或是删除标签，添加标签时会有默认命名，也可以双击标签自行给标签命名。
+* 添加标签完毕后即可选中某个标签，点击```Positive Point```或是```Negative Point```后的按钮即可开始交互式标注。
+* 点击```Finish Segment```按钮，即可结束当前所选标签下的标注，此时可点击左侧的橡皮擦等工具对标注结果进行精修。或者可重复以上步骤进行下一个对象的标注，否则可点击```Finish Scan```按钮，便会切换下一张图像。
+<p align="center">
+<img src="https://user-images.githubusercontent.com/48357642/187882400-8ee24469-6cb7-4c6a-acf8-df0e14e3f2a7.PNG" width="70.6%" height="20%">
+</p>
+
+* 关于精细修改标注工具的使用，详细可见[Slicer Segment editor](https://slicer.readthedocs.io/en/latest/user_guide/modules/segmenteditor.html)
+
+#### 5. 切换图像
+* 点击```Prev Scan```按钮可以切换上一张图像到当前视图框内。
+* 点击```Next Scan```按钮可以切换下一张图像到当前视图框内。
+<p align="center">
+<img src="https://user-images.githubusercontent.com/48357642/187882440-e1c3cc03-b79e-4ad8-9987-20af42c9ae01.PNG" width="70.6%" height="20%">
+</p>
+
+#### 6. 查看标注进程
+* 在```Progress```中的```Annotation Progress```后面的进度条中可以查看当前加载进来的图像标注进程。
+* 双击```Annotation Progress```下方表格中某一张图像文件名，便可以自动跳转到所选图像。
+<p align="center">
+<img src="https://user-images.githubusercontent.com/48357642/187882460-0eb0fc86-d9d7-4733-b812-85c62b1b9281.PNG" width="70.6%" height="20%">
+</p>
+<!-- </details> -->
+
+## <img src="https://user-images.githubusercontent.com/34859558/190046674-53e22678-7345-4bf1-ac0c-0cc99718b3dd.png" width="30"/> TODO
+未来，我们想在这几个方面来继续发展EISeg-Med3D，欢迎加入我们的开发者小组。
+- [ ] 在更大的椎骨数据集上进行训练，获取泛化性能更好的标注模型。
+- [ ] 开发在多个器官上训练的模型，从而获取能泛化到多器官的标注模型。
+
+## <img src="https://user-images.githubusercontent.com/34859558/188446853-6e32659e-8939-4e65-9282-68909a38edd7.png" width="30"/> License
+EISeg-Med3D 的 License 为 [Apache 2.0 license](LICENSE).
+
+
+## <img src="https://user-images.githubusercontent.com/34859558/188446803-06c54d50-f2aa-4a53-8e08-db2253df52fd.png" width="30"/> 致谢
+
+感谢 <a href="https://www.flaticon.com/free-icons/idea" title="idea icons"> Idea icons created by Vectors Market - Flaticon</a> 给我们提供了好看的图标
