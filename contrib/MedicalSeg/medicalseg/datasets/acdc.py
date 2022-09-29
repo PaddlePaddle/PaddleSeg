04c1dc45514d28a1871991343b7f2d1c6e84e8b9
sheyan21@126.com <45915150+YellowLight021@users.noreply.github.com>
2022-09-29 13:46:38 +0800

    the last changes

--- contrib/MedicalSeg/medicalseg/datasets/acdc.py
+++ contrib/MedicalSeg/medicalseg/datasets/acdc.py
@@ -19,9 +19,9 @@ import numpy as np
 sys.path.append(
     os.path.join(os.path.dirname(os.path.realpath(__file__)), "../.."))
 
+import paddle
 from medicalseg.cvlibs import manager
 from medicalseg.transforms import Compose
-import paddle
 
 URL = ' '  # todo: add coronavirus url
 
