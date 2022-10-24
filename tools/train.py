2430bda28adbef87afe70d3d1963ee7d77227308
Bobholamovic <mhlin425@whu.edu.cn>
2022-10-24 14:18:24 +0800

    Sync two train.py scripts

--- tools/train.py
+++ tools/train.py
@@ -136,7 +136,7 @@ def parse_args():
     parser.add_argument(
         '--device',
         dest='device',
-        help='Device place to be set, which can be gpu, xpu, npu, or cpu.',
+        help='Device place to be set, which can be gpu, xpu, npu, mlu or cpu.',
         default='gpu',
         choices=['cpu', 'gpu', 'xpu', 'npu'],
         type=str)
@@ -151,7 +151,6 @@ def parse_args():
 
 
 def main(args):
-
     if args.seed is not None:
         paddle.seed(args.seed)
         np.random.seed(args.seed)
@@ -170,6 +169,8 @@ def main(args):
         place = 'xpu'
     elif args.device == 'npu' and paddle.is_compiled_with_npu():
         place = 'npu'
+    elif args.device == 'mlu' and paddle.is_compiled_with_mlu():
+        place = 'mlu'
     else:
         place = 'cpu'
 
