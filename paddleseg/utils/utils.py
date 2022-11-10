ba00d354762ad7514ed4bbdd44a84645ea44ebc2
wuyefeilin <chenguowei01@baidu.com>
2022-11-10 15:56:28 +0800

    update pretrained model download process

--- paddleseg/utils/utils.py
+++ paddleseg/utils/utils.py
@@ -54,8 +54,10 @@ def download_pretrained_model(pretrained_model):
 
     pretrained_model = unquote(pretrained_model)
     savename = pretrained_model.split('/')[-1]
+    filename = 'model.pdparams'
     if not savename.endswith(('tgz', 'tar.gz', 'tar', 'zip')):
         savename = pretrained_model.split('/')[-2]
+        filename = pretrained_model.split('/')[-1]
     else:
         savename = savename.split('.')[0]
 
@@ -65,8 +67,9 @@ def download_pretrained_model(pretrained_model):
                 pretrained_model,
                 savepath=_dir,
                 extrapath=seg_env.PRETRAINED_MODEL_HOME,
-                extraname=savename)
-            pretrained_model = os.path.join(pretrained_model, 'model.pdparams')
+                extraname=savename,
+                filename=filename)
+            pretrained_model = os.path.join(pretrained_model, filename)
     return pretrained_model
 
 
