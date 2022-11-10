e328fadc5b6e7b41e2bfdde8de76d43b7890fa6d
zhengya01 <zhengya01@baidu.com>
2022-11-10 14:48:08 +0800

    fix serving cpp

--- test_tipc/test_serving_infer_cpp.sh
+++ test_tipc/test_serving_infer_cpp.sh
@@ -85,8 +85,8 @@ function func_serving(){
             last_status=${PIPESTATUS[0]}
             status_check $last_status "${clinet_cmd}" "${status_log}" "${model_name}" "${clinet_log_path}"
             eval "cat ${clinet_log_path}"
-            #ps ux | grep -i ${port_value} | awk '{print $2}' | xargs kill -s 9
-            ${python} -m paddle_serving_server.serve stop
+            ps ux | grep -i ${port_value} | awk '{print $2}' | xargs kill -s 9
+            #${python} -m paddle_serving_server.serve stop
             sleep 5s
         else
             cpp_server_log_path="${LOG_PATH}/cpp_server_gpu.log"
@@ -101,8 +101,8 @@ function func_serving(){
             last_status=${PIPESTATUS[0]}
             status_check $last_status "${clinet_cmd}" "${status_log}" "${model_name}" "${clinet_log_path}"
             eval "cat ${clinet_log_path}"
-            #ps ux | grep -i ${port_value} | awk '{print $2}' | xargs kill -s 9
-            ${python} -m paddle_serving_server.serve stop
+            ps ux | grep -i ${port_value} | awk '{print $2}' | xargs kill -s 9
+            #${python} -m paddle_serving_server.serve stop
             sleep 5s
         fi
     done
