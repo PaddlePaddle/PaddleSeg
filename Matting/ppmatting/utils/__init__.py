51a34cffc735cfbacf3d085f08f6635454337b6a
wuyefeilin <30919197+wuyefeilin@users.noreply.github.com>
2022-12-09 16:30:37 +0800

    [Matting][Enhancement] Add RVM Model  to Matting toolkit (#2809)

--- Matting/ppmatting/utils/__init__.py
+++ Matting/ppmatting/utils/__init__.py
@@ -1,2 +1,4 @@
 from .estimate_foreground_ml import estimate_foreground_ml
 from .utils import get_files, get_image_list, mkdir, load_pretrained_model
+from .video import VideoReader, VideoWriter
+from .export import get_input_spec
