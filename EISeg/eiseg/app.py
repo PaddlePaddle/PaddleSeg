31bd4894b241dc2f594f09330c17f23c6e168bd7
geoyee <geoyee@yeah.net>
2023-03-10 11:56:16 +0800

    [EISeg] Fix change save folder after open new folder

--- EISeg/eiseg/app.py
+++ EISeg/eiseg/app.py
@@ -1436,9 +1436,7 @@ class APP_EISeg(QMainWindow, Ui_EISeg):
         if len(imagePaths) == 0:
             return
         # 3.2 设置默认输出路径
-        if self.outputDir is None:
-            # 没设置为文件夹下的 label 文件夹
-            self.outputDir = osp.join(inputDir, "label")
+        self.outputDir = osp.join(inputDir, "label")
         if not osp.exists(self.outputDir):
             os.makedirs(self.outputDir)
         # 3.3 有重名图片，标签保留原来拓展名
