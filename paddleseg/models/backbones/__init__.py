4f9451dc14c91dde36d68adbe560156e34bb8c6c
teach <1250859983@qq.com>
2023-04-04 16:52:54 +0800

    solve conflict

--- paddleseg/models/backbones/__init__.py
+++ paddleseg/models/backbones/__init__.py
@@ -27,10 +27,6 @@ from .ghostnet import *
 from .cae import *
 from .top_transformer import *
 from .uhrnet import *
-<<<<<<< HEAD
-from .hrformer import *
-=======
 from .vit_adapter import *
 from .hrformer import *
 from .mscan import *
->>>>>>> 574be6fd86c5aa99dd7b60c746d4166def5698e0
