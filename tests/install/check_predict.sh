c5fec3c2fa4bc60661b67cea302ebcad752539ae
Ccc <52520497+juncaipeng@users.noreply.github.com>
2023-04-06 15:44:28 +0800

    Fix the root error of tools/predict.py

--- tests/install/check_predict.sh
+++ tests/install/check_predict.sh
@@ -1,5 +1,5 @@
 # check predicting
-python predict.py \
+python tools/predict.py \
     --config configs/quick_start/pp_liteseg_optic_disc_512x512_1k.yml \
     --model_path https://paddleseg.bj.bcebos.com/dygraph/optic_disc/pp_liteseg_optic_disc_512x512_1k/model.pdparams \
     --image_path docs/images/optic_test_image.jpg \
