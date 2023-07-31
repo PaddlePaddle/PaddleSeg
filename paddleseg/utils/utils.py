c833917b3942cce10c8f4eed8b46cc0c5c86e1da
ShawnNew <ncxinhanzhong@gmail.com>
2023-07-31 14:55:48 +0800

    [MLU] add mlu device config.

--- paddleseg/utils/utils.py
+++ paddleseg/utils/utils.py
@@ -68,7 +68,7 @@ def set_device(device):
         place = device
     elif 'npu' in device and paddle.is_compiled_with_custom_device('npu'):
         place = device
-    elif 'mlu' in device and paddle.is_compiled_with_mlu():
+    elif 'mlu' in device and paddle.is_compiled_with_custom_device('mlu'):
         place = device
     else:
         place = 'cpu'
