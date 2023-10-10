3fea7bb9b146441de559026802a736f70835ae5e
wuyefeilin <30919197+wuyefeilin@users.noreply.github.com>
2023-10-10 14:05:46 +0800

    Add analysis for val dataset (#3513)

--- paddleseg/core/__init__.py
+++ paddleseg/core/__init__.py
@@ -15,6 +15,7 @@
 from .train import train
 from .val import evaluate
 from .predict import predict
+from .analyse import analyse
 from . import infer
 
-__all__ = ['train', 'evaluate', 'predict']
+__all__ = ['train', 'evaluate', 'predict', 'analyse']
